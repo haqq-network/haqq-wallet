f3390e7dcbd683d7641e168a19c231eaae24975e
vivalaakam <viva.la.akam@gmail.com>
2022-08-02 12:53:27 +0300

    cleanup in app

--- src/app.tsx
+++ src/app.tsx
@@ -8,23 +8,27 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import * as Keychain from 'react-native-keychain';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from './screens/home';
 import {AuthContext} from './contexts/auth';
 import {DetailsScreen} from './screens/details';
 import {SplashScreen} from './screens/splash';
 import {LoginScreen} from './screens/login';
-import {PasswordScreen} from "./screens/password";
-import {Create1Screen} from "./screens/create-1";
-import {Create2Screen} from "./screens/create-2";
-import {Create3Screen} from "./screens/create-3";
+import {PasswordScreen} from './screens/password';
+import {Create1Screen} from './screens/create-1';
+import {Create2Screen} from './screens/create-2';
+import {Create3Screen} from './screens/create-3';
 
 const Stack = createNativeStackNavigator();
 
 export const App = () => {
+  const navigator = useNavigationContainerRef();
   const [credentials, setCredentials] = useState({
     authorized: false,
     loaded: false,
@@ -38,34 +42,34 @@ export const App = () => {
           loaded: true,
           ...creds,
         });
+
+        navigator.navigate('home');
       } else {
         setCredentials({
           authorized: false,
           loaded: true,
         });
+
+        navigator.navigate('login');
       }
     });
-  }, []);
+  }, [navigator]);
 
   return (
     <AuthContext.Provider value={credentials}>
-      <NavigationContainer>
-        {credentials.loaded && credentials.authorized ? (
-          <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name="home" component={HomeScreen}/>
-            <Stack.Group screenOptions={{presentation: 'modal'}}>
-              <Stack.Screen name="details" component={DetailsScreen}/>
-            </Stack.Group>
-          </Stack.Navigator>
-        ) : (
-          <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name="login" component={LoginScreen}/>
-            <Stack.Screen name="create-1" component={Create1Screen}/>
-            <Stack.Screen name="create-2" component={Create2Screen}/>
-            <Stack.Screen name="create-3" component={Create3Screen}/>
-            <Stack.Screen name="password" component={PasswordScreen}/>
-          </Stack.Navigator>
-        )}
+      <NavigationContainer ref={navigator}>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="splash" component={SplashScreen}/>
+          <Stack.Screen name="home" component={HomeScreen}/>
+          <Stack.Group screenOptions={{presentation: 'modal'}}>
+            <Stack.Screen name="details" component={DetailsScreen}/>
+          </Stack.Group>
+          <Stack.Screen name="login" component={LoginScreen}/>
+          <Stack.Screen name="create-1" component={Create1Screen}/>
+          <Stack.Screen name="create-2" component={Create2Screen}/>
+          <Stack.Screen name="create-3" component={Create3Screen}/>
+          <Stack.Screen name="password" component={PasswordScreen}/>
+        </Stack.Navigator>
       </NavigationContainer>
     </AuthContext.Provider>
   );
