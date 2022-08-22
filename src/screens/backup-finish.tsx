0d19c2d5dab7e04fae89de8c8417c5bbc4cc2c68
Andrey Makarov <viva.la.akam@gmail.com>
2022-08-22 12:20:17 +0300

    Backup screen animation (#6)

--- src/screens/backup-finish.tsx
+++ src/screens/backup-finish.tsx
@@ -3,13 +3,24 @@ import {CompositeScreenProps} from '@react-navigation/native';
 import {Button, ButtonVariant, Title} from '../components/ui';
 import {Container} from '../components/container';
 import {Spacer} from '../components/spacer';
+import Lottie from 'lottie-react-native';
+import {Dimensions} from 'react-native';
 
 type BackupFinishScreenProp = CompositeScreenProps<any, any>;
+
+const animationSize = Dimensions.get('window').width - 116;
 
 export const BackupFinishScreen = ({navigation}: BackupFinishScreenProp) => {
   return (
     <Container>
-      <Spacer />
+      <Spacer style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Lottie
+          style={{width: animationSize, height: animationSize}}
+          source={require('../../assets/animations/backup-success-animation.json')}
+          autoPlay
+          loop={false}
+        />
+      </Spacer>
       <Title>Сongratulations!</Title>
       <Title style={{marginBottom: 56}}>
         You've successfully protected your wallet.
