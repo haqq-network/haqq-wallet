e2703245bd77320d92838b091aeeeb5234a21db3
Andrey Makarov <viva.la.akam@gmail.com>
2022-08-29 14:44:02 +0300

    Use firebase (#4)
    * add distribution
    
    * fastlane init
    
    * version

--- android/app/src/main/java/com/haqq/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/haqq/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.haqq.newarchitecture.components;
+package com.haqq.testapp.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
