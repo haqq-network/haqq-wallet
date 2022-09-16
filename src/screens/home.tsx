9abacd96b24d1bd3ed0153c59c231381880091bb
Andrey Makarov <viva.la.akam@gmail.com>
2022-09-16 12:21:41 +0300

    navbar icons (#33)

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -7,6 +7,8 @@ import {HomeMarketScreen} from './home-market';
 import {HomeSwapScreen} from './home-swap';
 import {QrScannerButton} from '../components/qr-scanner-button';
 import {TabHeader} from '../components/tab-header';
+import {MarketIcon, SettingsIcon, SwapIcon, WalletIcon} from '../components/ui';
+import {GRAPHIC_BASE_2, GRAPHIC_GREEN_1} from '../variables';
 
 type HomeScreenProp = CompositeScreenProps<any, any>;
 const Tab = createBottomTabNavigator();
@@ -26,22 +28,43 @@ export const HomeScreen = ({}: HomeScreenProp) => {
           title: 'Wallet',
           headerTitle: 'Your wallets',
           headerRight: () => <QrScannerButton />,
+          tabBarIcon: ({focused}) => (
+            <WalletIcon color={focused ? GRAPHIC_GREEN_1 : GRAPHIC_BASE_2} />
+          ),
         }}
       />
       <Tab.Screen
         name="home-market"
         component={HomeMarketScreen}
-        options={{title: 'Market', headerTitle: 'Market screen'}}
+        options={{
+          title: 'Market',
+          headerTitle: 'Market screen',
+          tabBarIcon: ({focused}) => (
+            <MarketIcon color={focused ? GRAPHIC_GREEN_1 : GRAPHIC_BASE_2} />
+          ),
+        }}
       />
       <Tab.Screen
         name="home-swap"
         component={HomeSwapScreen}
-        options={{title: 'Swap', headerTitle: 'Swap screen'}}
+        options={{
+          title: 'Swap',
+          headerTitle: 'Swap screen',
+          tabBarIcon: ({focused}) => (
+            <SwapIcon color={focused ? GRAPHIC_GREEN_1 : GRAPHIC_BASE_2} />
+          ),
+        }}
       />
       <Tab.Screen
         name="home-settings"
         component={HomeSettingsScreen}
-        options={{title: 'Settings', headerTitle: 'Settings'}}
+        options={{
+          title: 'Settings',
+          headerTitle: 'Settings',
+          tabBarIcon: ({focused}) => (
+            <SettingsIcon color={focused ? GRAPHIC_GREEN_1 : GRAPHIC_BASE_2} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
