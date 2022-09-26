39763938e67db0351ebbb22416d9ff6e57cac8ec
vivalaakam <viva.la.akam@gmail.com>
2022-09-26 12:57:54 +0300

    input error

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import {StyleSheet, TextInput, TextInputProps} from 'react-native';
 import {TEXT_BASE_1} from '../../variables';
-import {LabeledBlock} from './labeled-block';
+import {LabelBlockVariant, LabeledBlock} from './labeled-block';
 
 export type InputProps = TextInputProps & {
   label: string;
+  error?: boolean;
   rightAction?: React.ReactNode;
 };
 
-export const Input = ({style, label, rightAction, ...props}: InputProps) => {
+export const Input = ({
+  style,
+  label,
+  rightAction,
+  error,
+  ...props
+}: InputProps) => {
   return (
     <LabeledBlock
       label={label}
       style={[page.wrapper, style]}
+      variant={error ? LabelBlockVariant.error : LabelBlockVariant.default}
       rightAction={rightAction}>
       <TextInput style={page.input} {...props} />
     </LabeledBlock>
