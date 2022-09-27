7f190bb0eb1669a624444980cb745f7761815123
Andrey Makarov <viva.la.akam@gmail.com>
2022-09-27 12:44:05 +0300

    refactor wallets view (#60)

--- src/components/wallets.tsx
+++ src/components/wallets.tsx
@@ -1,56 +1,19 @@
 import {useWallets} from '../contexts/wallets';
 import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {H2, PlusIcon} from './ui';
-import {Animated, Dimensions, PanResponder, View} from 'react-native';
+import {Animated, Dimensions, ScrollView, View} from 'react-native';
 import {GRAPHIC_BASE_1, MAGIC_CARD_HEIGHT} from '../variables';
 import {CarouselItem} from './carousel-item';
 import {WalletCard} from './wallet-card';
 import {WalletCreate} from './wallet-create';
 
 const cardWidth = Dimensions.get('window').width - 40;
-const cardHeight = cardWidth * MAGIC_CARD_HEIGHT;
 
 export const Wallets = () => {
   const wallets = useWallets();
   const [visibleRows, setVisibleRows] = useState(wallets.visible);
 
-  const progress = useRef(new Animated.Value(0)).current;
   const pan = useRef(new Animated.Value(0)).current;
-  const current = Animated.add(progress, pan);
-
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (event, gestureState) => {
-        const dist =
-          Math.max(Math.min(1, gestureState.dy / cardHeight), -1) * -1;
-
-        if (
-          progress._value + dist > 0 &&
-          progress._value + dist < wallets.visible.length
-        ) {
-          pan.setValue(dist);
-        }
-      },
-      onPanResponderRelease: (event, gestureState) => {
-        const dist =
-          Math.max(Math.min(1, gestureState.dy / cardHeight), -1) * -1;
-        const delta = Math.round(dist);
-        if (
-          progress._value + dist > 0 &&
-          progress._value + dist < wallets.visible.length
-        ) {
-          Animated.spring(pan, {
-            toValue: delta,
-            useNativeDriver: false,
-          }).start(() => {
-            progress.setValue(progress._value + delta);
-            pan.setValue(0);
-          });
-        }
-      },
-    }),
-  ).current;
 
   const updateWallets = useCallback(() => {
     setVisibleRows(wallets.visible);
@@ -65,70 +28,69 @@ export const Wallets = () => {
   }, [updateWallets, wallets]);
 
   return (
-    <View>
-      <Animated.View
-        {...panResponder.panHandlers}
+    <View style={{position: 'relative'}}>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        onScroll={({nativeEvent}) => {
+          pan.setValue(
+            nativeEvent.contentOffset.x / Dimensions.get('window').width,
+          );
+        }}
         style={{
           height: cardWidth * MAGIC_CARD_HEIGHT + 48,
           overflow: 'hidden',
-          position: 'relative',
         }}>
         {visibleRows.map((w, i) => (
-          <CarouselItem
-            height={cardWidth * MAGIC_CARD_HEIGHT}
-            index={i}
-            pan={current}
-            key={w.address}>
+          <CarouselItem index={i} pan={pan} key={w.address}>
             <WalletCard address={w.address} />
           </CarouselItem>
         ))}
-        <CarouselItem
-          height={cardWidth * MAGIC_CARD_HEIGHT}
-          index={visibleRows.length}
-          pan={current}>
+        <CarouselItem index={visibleRows.length} pan={pan}>
           <WalletCreate />
         </CarouselItem>
-        <View
-          style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            width: 20,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          {visibleRows.map((w, i) => (
-            <Animated.View
-              key={w.address}
-              style={{
-                width: 6,
-                height: 6,
-                backgroundColor: GRAPHIC_BASE_1,
-                borderRadius: 3,
-                margin: 3,
-                opacity: current.interpolate({
-                  inputRange: [i - 1, i, i + 1],
-                  outputRange: [0.5, 1, 0.5],
-                  extrapolate: 'clamp',
-                }),
-              }}
-            />
-          ))}
+      </ScrollView>
+      <View
+        style={{
+          height: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'row',
+          marginTop: -20,
+        }}>
+        {visibleRows.map((w, i) => (
           <Animated.View
+            key={w.address}
             style={{
-              width: 12,
-              height: 12,
-              opacity: current.interpolate({
-                inputRange: [wallets.getSize() - 1, wallets.getSize()],
-                outputRange: [0.5, 1],
+              width: 6,
+              height: 6,
+              backgroundColor: GRAPHIC_BASE_1,
+              borderRadius: 3,
+              margin: 3,
+              opacity: pan.interpolate({
+                inputRange: [i - 1, i, i + 1],
+                outputRange: [0.5, 1, 0.5],
                 extrapolate: 'clamp',
               }),
-            }}>
-            <PlusIcon color={GRAPHIC_BASE_1} width={12} height={12} />
-          </Animated.View>
-        </View>
-      </Animated.View>
+            }}
+          />
+        ))}
+        <Animated.View
+          style={{
+            width: 12,
+            height: 12,
+            opacity: pan.interpolate({
+              inputRange: [wallets.getSize() - 1, wallets.getSize()],
+              outputRange: [0.5, 1],
+              extrapolate: 'clamp',
+            }),
+          }}>
+          <PlusIcon color={GRAPHIC_BASE_1} width={12} height={12} />
+        </Animated.View>
+      </View>
       <H2
         style={{marginVertical: 12, textAlign: 'left', paddingHorizontal: 20}}>
         Transactions
