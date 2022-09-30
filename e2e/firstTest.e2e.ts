d4073301fde08b7bc5f149bc0e7c78530faae266
vivalaakam <viva.la.akam@gmail.com>
2022-09-30 14:07:47 +0300

    upd

---
+++ e2e/firstTest.e2e.ts
@@ -0,0 +1,15 @@
+import {by, device, element, expect} from 'detox';
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+});
