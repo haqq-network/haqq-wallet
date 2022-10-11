a1f4d5e140f507a167e75f964317aa96dc11727e
vivalaakam <viva.la.akam@gmail.com>
2022-10-11 12:03:02 +0300

    tighten the nuts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'import'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -10,6 +10,7 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'import/no-default-export': 'error',
         'react-native/no-unused-styles': 2,
         'react-native/split-platform-components': 2,
         'react-native/no-inline-styles': 2,
