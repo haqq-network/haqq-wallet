970e18ec013c687daa412078ff06b1d6b1ecfd96
Dmitriy Vasilev <raoffonom@icloud.com>
2022-10-13 13:01:34 +0700

    Fix for Lottie comp

--- src/screens/backup-finish.tsx
+++ src/screens/backup-finish.tsx
@@ -19,10 +19,10 @@ export const BackupFinishScreen = ({navigation}: BackupFinishScreenProp) => {
     <Container>
       <Spacer style={page.container}>
         <LottieWrap
-          style={{width: animationSize, height: animationSize}}
           source={require('../../assets/animations/backup-success-animation.json')}
           autoPlay
           loop={false}
+          style={{width: animationSize, height: animationSize}}
         />
       </Spacer>
       <Text t4>Congratulations!</Text>
