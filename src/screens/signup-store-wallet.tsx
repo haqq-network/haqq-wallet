5c69358a65f1ca19d7867e1c4a22ce0cdcb88a70
Andrey Makarov <viva.la.akam@gmail.com>
2022-10-13 12:20:24 +0300

    set animation timings (#179)

--- src/screens/signup-store-wallet.tsx
+++ src/screens/signup-store-wallet.tsx
@@ -21,7 +21,7 @@ export const SignupStoreWalletScreen = ({
   }, [app]);
 
   useEffect(() => {
-    requestAnimationFrame(() => {
+    setTimeout(() => {
       const actions = [sleep(4000)];
 
       actions.push(
@@ -36,7 +36,7 @@ export const SignupStoreWalletScreen = ({
       Promise.all(actions).then(() => {
         navigation.navigate(route.params.nextScreen ?? 'onboardingFinish');
       });
-    });
+    }, 350);
   }, [navigation, route, wallets]);
 
   return <View />;
