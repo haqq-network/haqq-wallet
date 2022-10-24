e0c4f4b3a3b9e940a4f24a029143b6efd7f0f4d3
vivalaakam <viva.la.akam@gmail.com>
2022-10-24 21:18:23 +0300

    fix buttons

--- src/components/wallet-create.tsx
+++ src/components/wallet-create.tsx
@@ -37,23 +37,33 @@ export const WalletCreate = ({}: BalanceProps) => {
         style={page.create}
       />
       <View style={page.buttons}>
-        {isLedgerEnabled && (
+        {isLedgerEnabled ? (
+          <>
+            <Button
+              variant={ButtonVariant.second}
+              title="Connect"
+              style={page.create}
+              onPress={() => {
+                navigation.navigate('ledger');
+              }}
+            />
+            <Button
+              title="Import"
+              style={page.create}
+              onPress={() => {
+                navigation.navigate('restore');
+              }}
+            />
+          </>
+        ) : (
           <Button
-            variant={ButtonVariant.second}
-            title="Connect"
+            title="Import  an existing one"
             style={page.create}
             onPress={() => {
-              navigation.navigate('ledger');
+              navigation.navigate('restore');
             }}
           />
         )}
-        <Button
-          title="Import  an existing one"
-          style={page.create}
-          onPress={() => {
-            navigation.navigate('restore');
-          }}
-        />
       </View>
     </View>
   );
