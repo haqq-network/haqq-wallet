b351ac7ad8d79f0a9af3fac827c43ada80eb65e9
Dmitriy Vasilev <raoffonom@icloud.com>
2022-10-24 20:58:29 +0700

    status bar

--- src/components/ui/status-bar-color.tsx
+++ src/components/ui/status-bar-color.tsx
@@ -5,11 +5,13 @@ import {BG_1} from '../../variables';
 
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
 
-export const StatusBarColor = ({backgroundColor = BG_1, ...props}) => (
-  <View style={[page.statusBar, {backgroundColor}]}>
-    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-  </View>
-);
+export const StatusBarColor = ({backgroundColor = BG_1, ...props}) => {
+  return (
+    <View style={[page.statusBar, {backgroundColor}]}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+};
 
 const page = StyleSheet.create({
   statusBar: {
