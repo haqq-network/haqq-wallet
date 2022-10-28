b7fa0f536a4d43b639396d73872606dead5e7dba
Andrey Makarov <viva.la.akam@gmail.com>
2022-10-28 12:02:13 +0300

    fix crash on android (#251)
    Co-authored-by: Server Serverlesskiy <raoffonom@icloud.com>

--- src/screens/ledger-verify.tsx
+++ src/screens/ledger-verify.tsx
@@ -2,33 +2,21 @@ import React, {useCallback} from 'react';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {RootStackParamList} from '../types';
-import {useWallets} from '../contexts/wallets';
 import {LedgerVerify} from '../components/ledger-verify';
-import {captureException} from '../helpers';
 
 export const LedgerVerifyScreen = () => {
-  const wallets = useWallets();
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   const route = useRoute<RouteProp<RootStackParamList, 'ledgerVerify'>>();
 
   const onDone = useCallback(
-    async (params: {address: string}) => {
-      try {
-        await wallets.addWalletFromLedger(
-          {
-            address: params.address,
-            deviceId: route.params.deviceId,
-            deviceName: route.params.deviceName,
-          },
-          route.params.deviceName,
-        );
-        navigation.navigate(route.params.nextScreen ?? 'ledgerFinish');
-      } catch (e) {
-        captureException(e, 'LedgerVerifyScreen');
-      }
+    (params: {address: string}) => {
+      navigation.navigate(route.params.nextScreen ?? 'ledgerStore', {
+        address: params.address,
+        deviceId: route.params.deviceId,
+        deviceName: route.params.deviceName,
+      });
     },
     [
-      wallets,
       route.params.deviceId,
       route.params.deviceName,
       route.params.nextScreen,
