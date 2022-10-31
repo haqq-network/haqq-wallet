e766296425f4754ad101f91867f448e89c3fcf89
Andrey Makarov <viva.la.akam@gmail.com>
2022-10-31 12:14:50 +0300

    fix current network error (#262)
    Co-authored-by: Server Serverlesskiy <raoffonom@icloud.com>

--- src/components/ui/data-content-splitted.tsx
+++ src/components/ui/data-content-splitted.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {StyleSheet, View, ViewStyle} from 'react-native';
 import {Text} from './text';
 import {TEXT_BASE_1, TEXT_BASE_2} from '../../variables';
+import Clipboard from '@react-native-clipboard/clipboard';
+import {app} from '../../contexts/app';
 
 export type DataContentSplittedProps = {
   to: string[];
@@ -16,9 +18,14 @@ export const DataContentSplitted = ({
   style,
   reversed = false,
 }: DataContentSplittedProps) => {
+  const onPress = useCallback(() => {
+    Clipboard.setString(to.join(''));
+    app.emit('notification', 'Copied');
+  }, [to]);
+
   return (
     <View style={[reversed && page.reverse, style]}>
-      <Text t11 style={page.address}>
+      <Text t11 style={page.address} onPress={onPress}>
         <Text t11 style={page.address}>
           {to[0]}
         </Text>
