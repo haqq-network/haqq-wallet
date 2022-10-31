91af40d8dd5a21710926ca8c7090e230ece3e578
vivalaakam <viva.la.akam@gmail.com>
2022-10-31 12:13:11 +0300

    refactor agreement screens

--- src/components/restore-agreement/restore-agreement.tsx
+++ src/components/restore-agreement/restore-agreement.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Button, ButtonVariant, PopupContainer, Text} from '../ui';
+import {Button, ButtonVariant, LottieWrap, PopupContainer, Text} from '../ui';
 import {TEXT_BASE_2} from '../../variables';
-import {LottieWrap} from '../lottie';
+import {ratio, windowWidth} from '../../helpers';
 import {Terms} from '../ui/terms';
 import {getText, I18N} from '../../i18n';
 
-export type LedgerAgreementProps = {
+export type RestoreAgreementProps = {
   onDone: () => void;
+  testID?: string;
 };
 
-export const LedgerAgreement = ({onDone}: LedgerAgreementProps) => {
+export const RestoreAgreement = ({onDone, testID}: RestoreAgreementProps) => {
   return (
     <>
       <View style={page.animation}>
         <LottieWrap
-          source={require('../../../assets/animations/ledger-agreement.json')}
+          source={require('../../../assets/animations/recover-animation.json')}
           style={page.image}
           autoPlay
           loop
         />
       </View>
-      <PopupContainer style={page.container}>
+      <PopupContainer style={page.container} testID={testID}>
         <Text t4 style={page.title}>
-          {getText(I18N.ledgerAgreementTitle)}
+          {getText(I18N.restoreAgreementTitle)}
         </Text>
         <Text t11 style={page.disclaimer}>
-          {getText(I18N.ledgerAgreementText)}
+          {getText(I18N.restoreAgreementText)}
         </Text>
         <Button
           style={page.submit}
           variant={ButtonVariant.contained}
-          title={getText(I18N.ledgerAgreementAgree)}
+          title={getText(I18N.restoreAgreementAgree)}
+          testID={`${testID}_agree`}
           onPress={onDone}
         />
         <Terms style={page.agreement} />
@@ -49,7 +51,7 @@ const page = StyleSheet.create({
     left: 0,
     right: 0,
     height: 200,
-    top: 55,
+    top: 12,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -57,9 +59,11 @@ const page = StyleSheet.create({
     marginBottom: 4,
     marginHorizontal: 20,
     textAlign: 'center',
+    top: 50,
   },
   disclaimer: {
-    marginBottom: 84,
+    top: 50,
+    marginBottom: 154,
     textAlign: 'center',
     color: TEXT_BASE_2,
     marginHorizontal: 20,
@@ -69,5 +73,10 @@ const page = StyleSheet.create({
     marginHorizontal: 20,
     marginBottom: 16,
   },
-  image: {height: 310},
+  image: {
+    top: 10,
+    width: windowWidth,
+    height: 362 * ratio,
+    alignSelf: 'center',
+  },
 });
