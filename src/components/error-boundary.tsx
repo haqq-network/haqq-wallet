61e4948a296e9869be4512ea89c1ad00a49a61d5
Andrey Makarov <viva.la.akam@gmail.com>
2022-11-03 14:20:36 +0300

    refactor modals (#283)

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,40 @@
+import React, {PureComponent} from 'react';
+import {Text, View} from 'react-native';
+
+export type ErrorBoundaryProps = {
+  fallback: React.ReactNode;
+};
+
+export class ErrorBoundary extends PureComponent {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {error: null, errorInfo: null};
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    console.log(error, errorInfo);
+
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <View>
+          <Text>Something went wrong.</Text>
+          <Text>
+            {this.state.error && this.state.error.toString()}
+            {'\n'}
+            {this.state.errorInfo.componentStack}
+          </Text>
+        </View>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }
+}
