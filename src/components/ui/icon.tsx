6d4415b56f5a27e43f27ca2856d330186e330142
Andrey Makarov <viva.la.akam@gmail.com>
2022-11-09 13:52:27 +0300

    assets (#314)

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -3,10 +3,7 @@ import {Image, ImageProps} from 'react-native';
 import {IconsName} from '../../types';
 
 const icons = {
-  'arrow-back': require('../../../assets/images/arrow-back.png'),
   clear: require('../../../assets/images/clear.png'),
-  'face-id': require('../../../assets/images/face-id.png'),
-  'touch-id': require('../../../assets/images/face-id.png'),
 };
 
 export type IconProps = {
