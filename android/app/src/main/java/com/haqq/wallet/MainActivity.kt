1b7924c1dd82eee4840a46e0971e4bec9910d838
vivalaakam <viva.la.akam@gmail.com>
2022-11-14 12:05:47 +0400

    move to kotlin

---
+++ android/app/src/main/java/com/haqq/wallet/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.haqq.wallet
+
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.ReactRootView
+import com.haqq.wallet.MainActivity.MainActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
+
+class MainActivity : ReactActivity() {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String? {
+        return "haqq"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this, true)
+        super.onCreate(null)
+    }
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. There the RootView is created and
+     * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
+     * (Paper).
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return MainActivityDelegate(this, mainComponentName)
+    }
+
+    class MainActivityDelegate(activity: ReactActivity?, mainComponentName: String?) :
+        ReactActivityDelegate(activity, mainComponentName) {
+        override fun createRootView(): ReactRootView {
+            val reactRootView = ReactRootView(context)
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED)
+            return reactRootView
+        }
+
+        override fun isConcurrentRootEnabled(): Boolean {
+            // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+            // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        }
+    }
+}
