b78d376d37d3e900fa17ff90035e41085fa23cd2
vivalaakam <viva.la.akam@gmail.com>
2022-11-14 11:58:15 +0400

    refactor

--- src/components/modals/hide.tsx
+++ src/components/modals/hide.tsx
@@ -3,20 +3,24 @@ import React, {useEffect} from 'react';
 import {StatusBar, StyleSheet, View} from 'react-native';
 
 import {
+  IS_ANDROID,
   LIGHT_BG_1,
+  LIGHT_GRAPHIC_BASE_3,
   LIGHT_GRAPHIC_GREEN_2,
   LIGHT_TEXT_BASE_3,
 } from '../../variables';
-import {ISLMCirclesIcon, Text} from '../ui';
+import {Icon, Text} from '../ui';
 
 export const HideModal = () => {
   useEffect(() => {
-    StatusBar.setBackgroundColor(LIGHT_GRAPHIC_GREEN_2);
-    return () => StatusBar.setBackgroundColor(LIGHT_BG_1);
+    if (IS_ANDROID) {
+      StatusBar.setBackgroundColor(LIGHT_GRAPHIC_GREEN_2);
+      return () => StatusBar.setBackgroundColor(LIGHT_BG_1);
+    }
   }, []);
   return (
     <View style={page.container}>
-      <ISLMCirclesIcon />
+      <Icon xxl name="logo" color={LIGHT_GRAPHIC_BASE_3} />
       <Text t2 style={page.text}>
         ISLM Wallet
       </Text>
