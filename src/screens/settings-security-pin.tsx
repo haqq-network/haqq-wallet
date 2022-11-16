cfa4a3c6eaaec77b6e333f912e3e9bdc9ea080b0
vivalaakam <viva.la.akam@gmail.com>
2022-11-16 15:49:03 +0400

    review

--- src/screens/settings-security-pin.tsx
+++ src/screens/settings-security-pin.tsx
@@ -3,10 +3,10 @@ import React, {useCallback, useRef, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 
+import {Pin, PinInterface} from '@app/components/pin';
+import {hideModal, showModal} from '@app/helpers';
 import {useApp, useWallets} from '@app/hooks';
 
-import {Pin, PinInterface} from '../components/pin/pin';
-import {hideModal, showModal} from '../helpers/modal';
 import {RootStackParamList} from '../types';
 
 export const SettingsSecurityPinScreen = () => {
