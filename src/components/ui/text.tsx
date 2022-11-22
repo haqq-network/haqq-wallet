8d2435efd378646025a7560fb3f7d37156dfe975
Andrey Makarov <viva.la.akam@gmail.com>
2022-11-22 12:30:46 +0400

    Update text.tsx

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -15,9 +15,7 @@ import {createTheme} from '@app/helpers';
 import {I18N, getText} from '@app/i18n';
 import {FontT} from '@app/types';
 
-export type TextValue =
-  | {children: React.ReactNode; i18n: undefined}
-  | {i18n: I18N; children: undefined};
+export type TextValue = {children: React.ReactNode} | {i18n: I18N};
 
 export type TextProps = Omit<RNTextProps, 'style' | 'children'> & {
   t0?: boolean;
