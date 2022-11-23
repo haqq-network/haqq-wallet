950c3b50e11357e14b8fab271aad758aa4498323
vivalaakam <viva.la.akam@gmail.com>
2022-11-23 11:04:19 +0400

    fix types

--- src/helpers/create-theme.ts
+++ src/helpers/create-theme.ts
@@ -3,26 +3,28 @@ import {ImageStyle, TextStyle, ViewStyle} from 'react-native';
 import {Color, getColor} from '@app/colors';
 import {app} from '@app/contexts';
 
+type Style =
+  | ViewStyle
+  | TextStyle
+  | ImageStyle
+  | {
+      primary: string;
+      background: string;
+      card: string;
+      text: string;
+      border: string;
+      notification: string;
+    };
+
 type NamedStyles<T> = {
-  [P in keyof T]:
-    | ViewStyle
-    | TextStyle
-    | ImageStyle
-    | {
-        primary: string;
-        background: string;
-        card: string;
-        text: string;
-        border: string;
-        notification: string;
-      };
+  [P in keyof T]: Style;
 };
 
 export function createTheme<T extends NamedStyles<T> | NamedStyles<any>>(
   styles: T | NamedStyles<T>,
 ): T {
   const style = {};
-  const cache = new Map();
+  const cache: Map<string, Style> = new Map();
   const keys = Object.keys(styles);
   const styled = Object.keys(Color);
 
@@ -31,17 +33,17 @@ export function createTheme<T extends NamedStyles<T> | NamedStyles<any>>(
       get() {
         const k = `${key}_${app.getTheme()}`;
         if (!cache.has(k)) {
-          const res =
-            styles[key] instanceof Object
-              ? Object.entries(styles[key]).reduce((memo, [prop, value]) => {
-                  return {
-                    ...memo,
-                    [prop]: styled.includes(value as string)
-                      ? getColor(value as Color)
-                      : value,
-                  };
-                }, {})
-              : styles[key];
+          const res = Object.entries(styles[key]).reduce(
+            (memo, [prop, value]) => {
+              return {
+                ...memo,
+                [prop]: styled.includes(value as string)
+                  ? getColor(value as Color)
+                  : value,
+              };
+            },
+            {},
+          );
 
           cache.set(k, res);
         }
@@ -51,5 +53,5 @@ export function createTheme<T extends NamedStyles<T> | NamedStyles<any>>(
     });
   });
 
-  return style as U;
+  return style as T;
 }
