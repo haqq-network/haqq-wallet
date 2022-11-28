bba54f510f45f10c0e553079f7015826e9e09fc5
Andrey Makarov <viva.la.akam@gmail.com>
2022-11-28 10:29:32 +0400

    withdrawDelegatorReward (#460)

--- src/screens/staking-info.tsx
+++ src/screens/staking-info.tsx
@@ -1,15 +1,69 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 
 import {RouteProp, useRoute} from '@react-navigation/native';
 
 import {StakingInfo} from '@app/components/staking-info';
-import {useTypedNavigation} from '@app/hooks';
+import {app} from '@app/contexts';
+import {useTypedNavigation, useWallets} from '@app/hooks';
 import {StakingMetadata} from '@app/models/staking-metadata';
+import {Cosmos} from '@app/services/cosmos';
 import {RootStackParamList} from '@app/types';
 
 export const StakingInfoScreen = () => {
+  const wallets = useWallets();
   const route = useRoute<RouteProp<RootStackParamList, 'stakingInfo'>>();
   const navigation = useTypedNavigation();
+  const [withdrawDelegatorRewardProgress, setWithdrawDelegatorRewardProgress] =
+    useState(false);
+  const cosmos = useRef(new Cosmos(app.provider!)).current;
+  const [rewards, setRewards] = useState<Realm.Results<StakingMetadata> | null>(
+    null,
+  );
+
+  useEffect(() => {
+    const r = StakingMetadata.getRewardsForValidator(
+      route.params.validator.operator_address,
+    );
+
+    const subscription = () => {
+      setRewards(
+        StakingMetadata.getRewardsForValidator(
+          route.params.validator.operator_address,
+        ),
+      );
+    };
+
+    r.addListener(subscription);
+    setRewards(r);
+    return () => {
+      r.removeListener(subscription);
+    };
+  }, [route.params.validator.operator_address]);
+
+  const onWithdrawDelegatorReward = useCallback(() => {
+    if (rewards?.length) {
+      setWithdrawDelegatorRewardProgress(true);
+      const delegators = new Set(rewards.map(r => r.delegator));
+
+      Promise.all(
+        wallets.visible
+          .filter(w => delegators.has(w.cosmosAddress))
+          .map(w =>
+            cosmos.withdrawDelegatorReward(
+              w.address,
+              route.params.validator.operator_address,
+            ),
+          ),
+      )
+        .then((...resp) => {
+          console.log(JSON.stringify(resp));
+          rewards.forEach(r => StakingMetadata.remove(r.hash));
+        })
+        .finally(() => {
+          setWithdrawDelegatorRewardProgress(false);
+        });
+    }
+  }, [cosmos, rewards, route.params.validator.operator_address, wallets]);
 
   const onDelegate = useCallback(() => {
     navigation.push('stakingDelegate', {
@@ -29,10 +83,13 @@ export const StakingInfoScreen = () => {
 
   return (
     <StakingInfo
+      withdrawDelegatorRewardProgress={withdrawDelegatorRewardProgress}
       validator={route.params.validator}
       onDelegate={onDelegate}
       onUnDelegate={onUnDelegate}
+      onWithdrawDelegatorReward={onWithdrawDelegatorReward}
       delegations={delegated}
+      rewards={rewards}
     />
   );
 };
