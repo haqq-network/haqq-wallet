fb11d64c562e2365de882922ab5d6b378a1f05ea
Server Serverlesskiy <raoffonom@icloud.com>
2022-12-01 17:36:10 +0700

    refactor(ValidatorInfo): screens refactor (#526)
    * Correcting layout info stacking
    
    - Translate notifications
    - Code standart fixes
    - 16 icon size
    - Badge fix
    
    * refactor(ValidatorInfo): fix commnets
    
    - `sendNotification` modal helper
    - mini refactoring

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,20 +5,29 @@ import {StyleProp, View, ViewStyle} from 'react-native';
 import {Color, getColor} from '@app/colors';
 import {Text} from '@app/components/ui/text';
 import {createTheme} from '@app/helpers';
+import {I18N} from '@app/i18n';
 
 export type BadgeProps = {
-  text: string;
-  color: string;
+  text?: string;
+  i18n?: I18N;
+  labelColor: string | Color;
+  textColor?: string | Color;
   style?: StyleProp<ViewStyle>;
 };
-export const Badge = ({text, color, style}: BadgeProps) => {
+export const Badge = ({
+  text,
+  i18n,
+  labelColor,
+  textColor = Color.textBase3,
+  style,
+}: BadgeProps) => {
   const container = useMemo(
-    () => [styles.container, {backgroundColor: color}, style],
-    [color, style],
+    () => [styles.container, {backgroundColor: getColor(labelColor)}, style],
+    [labelColor, style],
   );
   return (
     <View style={container}>
-      <Text t13 color={getColor(Color.textBase3)}>
+      <Text t13 color={textColor} i18n={i18n}>
         {text}
       </Text>
     </View>
