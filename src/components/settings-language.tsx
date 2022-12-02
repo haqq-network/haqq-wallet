09f80dc18dba8c85eb4444542fcb30f337346ac5
Samandar <samandar8939522@gmail.com>
2022-12-02 16:20:06 +0500

    refactor: settings-language

---
+++ src/components/settings-language.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {Color} from '@app/colors';
+import {DataContent, IconButton, PopupContainer} from '@app/components/ui';
+import {Icon} from '@app/components/ui';
+import {createTheme} from '@app/helpers';
+import {AppLanguage} from '@app/types';
+
+type SettingsLanguageProps = {
+  updateLanguage: (lang: AppLanguage) => void;
+  language: AppLanguage;
+};
+
+export const SettingsLanguage = ({
+  updateLanguage,
+  language,
+}: SettingsLanguageProps) => {
+  return (
+    <PopupContainer style={styles.container}>
+      <IconButton
+        style={styles.button}
+        onPress={() => {
+          updateLanguage(AppLanguage.ar);
+        }}>
+        <DataContent title="Arabic" subtitle="العربية" />
+        {language === AppLanguage.ar && (
+          <Icon name="check" color={Color.graphicGreen1} i24 />
+        )}
+      </IconButton>
+      <IconButton
+        style={styles.button}
+        onPress={() => {
+          updateLanguage(AppLanguage.en);
+        }}>
+        <DataContent title="English" subtitle="English" />
+        {language === AppLanguage.en && (
+          <Icon name="check" color={Color.graphicGreen1} i24 />
+        )}
+      </IconButton>
+    </PopupContainer>
+  );
+};
+
+const styles = createTheme({
+  container: {
+    marginHorizontal: 20,
+  },
+  button: {
+    paddingVertical: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
--- src/screens/settings-language.tsx
+++ src/screens/settings-language.tsx
@@ -1,17 +1,8 @@
 import React, {useState} from 'react';
 
-import {StyleSheet} from 'react-native';
-
+import {SettingsLanguage} from '@app/components/settings-language';
 import {useApp} from '@app/hooks';
-
-import {
-  CheckIcon,
-  DataContent,
-  IconButton,
-  PopupContainer,
-} from '../components/ui';
-import {AppLanguage} from '../types';
-import {LIGHT_GRAPHIC_GREEN_1} from '../variables';
+import {AppLanguage} from '@app/types';
 
 export const SettingsLanguageScreen = () => {
   const app = useApp();
@@ -23,42 +14,6 @@ export const SettingsLanguageScreen = () => {
   };
 
   return (
-    <PopupContainer style={page.container}>
-      <IconButton
-        style={page.button}
-        onPress={() => {
-          updateLanguage(AppLanguage.ar);
-        }}>
-        <DataContent title="Arabic" subtitle="العربية" />
-        {language === AppLanguage.ar && (
-          <CheckIcon color={LIGHT_GRAPHIC_GREEN_1} style={page.icon} />
-        )}
-      </IconButton>
-      <IconButton
-        style={page.button}
-        onPress={() => {
-          updateLanguage(AppLanguage.en);
-        }}>
-        <DataContent title="English" subtitle="English" />
-        {language === AppLanguage.en && (
-          <CheckIcon color={LIGHT_GRAPHIC_GREEN_1} style={page.icon} />
-        )}
-      </IconButton>
-    </PopupContainer>
+    <SettingsLanguage language={language} updateLanguage={updateLanguage} />
   );
 };
-
-const page = StyleSheet.create({
-  container: {
-    marginHorizontal: 20,
-  },
-  button: {
-    paddingVertical: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  icon: {
-    width: 24,
-    height: 24,
-  },
-});
