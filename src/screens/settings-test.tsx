d107a27b0f5284492e37b6ae69f7fcfc97b2d88b
Andrey Makarov <viva.la.akam@gmail.com>
2022-12-02 15:33:00 +0400

    notifications ios

--- src/screens/settings-test.tsx
+++ src/screens/settings-test.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
 
-import {SettingsTest} from '@app/components/settings-test';
+import messaging from '@react-native-firebase/messaging';
+import {View} from 'react-native';
+
+import {Button, ButtonVariant} from '@app/components/ui';
+import {createTheme} from '@app/helpers';
+
+messaging().onMessage(async remoteMessage => {
+  console.log('onMessage', remoteMessage);
+});
+
+messaging().onNotificationOpenedApp(remoteMessage => {
+  console.log('onNotificationOpenedApp', remoteMessage);
+});
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('setBackgroundMessageHandler', remoteMessage);
+});
+
+messaging()
+  .getInitialNotification()
+  .then(remoteMessage => {
+    if (remoteMessage) {
+      console.log('getInitialNotification', remoteMessage);
+    }
+  });
 
 export const SettingsTestScreen = () => {
-  return <SettingsTest />;
+  const onPressRequestPermissions = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+
+      const token = await messaging().getToken();
+      console.log(token);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Request permissions"
+        onPress={onPressRequestPermissions}
+        variant={ButtonVariant.contained}
+      />
+    </View>
+  );
 };
+
+const styles = createTheme({
+  container: {
+    paddingHorizontal: 20,
+  },
+});
