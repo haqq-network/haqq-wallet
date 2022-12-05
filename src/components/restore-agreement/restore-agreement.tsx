13bd58388e3f5242ec48914e17b56156732bce01
Max <maskalyov.maks@gmail.com>
2022-12-05 18:22:05 +0300

    refactor code

--- src/components/restore-agreement/restore-agreement.tsx
+++ src/components/restore-agreement/restore-agreement.tsx
@@ -13,7 +13,7 @@ import {
 } from '@app/components/ui';
 import {createTheme, windowHeight, windowWidth} from '@app/helpers';
 import {useTheme} from '@app/hooks';
-import {I18N, getText} from '@app/i18n';
+import {I18N} from '@app/i18n';
 import {AppTheme} from '@app/types';
 
 // import {Terms} from '../ui/terms';
@@ -45,21 +45,19 @@ export const RestoreAgreement = ({onDone, testID}: RestoreAgreementProps) => {
           loop
         />
       </View>
-      <Text t4 center style={styles.title}>
-        {getText(I18N.restoreAgreementTitle)}
-      </Text>
+      <Text t4 center i18n={I18N.restoreAgreementTitle} style={styles.title} />
       <Text
         t11
         center
         style={styles.disclaimer}
-        color={getColor(Color.textBase2)}>
-        {getText(I18N.restoreAgreementText)}
-      </Text>
+        color={getColor(Color.textBase2)}
+        i18n={I18N.restoreAgreementText}
+      />
       <Spacer />
       <Button
         style={styles.submit}
         variant={ButtonVariant.contained}
-        title={getText(I18N.restoreAgreementAgree)}
+        i18n={I18N.restoreAgreementAgree}
         testID={`${testID}_agree`}
         onPress={onDone}
       />
