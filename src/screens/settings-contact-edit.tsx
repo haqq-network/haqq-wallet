a64bbf47688ccb04ce60799d7c0cc18134b1a20d
Samandar <samandar8939522@gmail.com>
2022-12-05 18:13:01 +0500

    fix: code conduct

--- src/screens/settings-contact-edit.tsx
+++ src/screens/settings-contact-edit.tsx
@@ -2,13 +2,13 @@ import React, {useState} from 'react';
 
 import {Alert} from 'react-native';
 
+import {Color} from '@app/colors';
 import {ActionsSheet} from '@app/components/actions-sheet';
 import {SettingsAddressBookEdit} from '@app/components/settings-address-book-edit';
 import {CustomHeader} from '@app/components/ui';
 import {useContacts, useTypedNavigation} from '@app/hooks';
 import {useTypedRoute} from '@app/hooks/use-typed-route';
 import {I18N, getText} from '@app/i18n';
-import {LIGHT_GRAPHIC_GREEN_1} from '@app/variables';
 
 export const SettingsContactEditScreen = () => {
   const {name, address, isCreate} =
@@ -35,9 +35,9 @@ export const SettingsContactEditScreen = () => {
     const DelateContact = getText(I18N.settingsContactEditDeleteContact);
     const Sure = getText(I18N.settingsContactEditSure);
     const Delete = getText(I18N.settingsContactEditDelete);
-    const Cencel = getText(I18N.cancel);
+    const Cancel = getText(I18N.cancel);
     Alert.alert(DelateContact, Sure, [
-      {text: Cencel, style: 'cancel'},
+      {text: Cancel, style: 'cancel'},
       {
         text: Delete,
         style: 'destructive',
@@ -79,15 +79,15 @@ export const SettingsContactEditScreen = () => {
   return (
     <>
       <CustomHeader
-        title={getText(I18N.settingsContactEditHeaderTitle)}
+        i18nTitle={I18N.settingsContactEditHeaderTitle}
         onPressLeft={onPressLeft}
         iconLeft={isEdit ? undefined : 'arrow_back'}
-        textLeft={getText(I18N.cancel)}
-        textRight={isEdit ? getText(I18N.save) : getText(I18N.edit)}
+        i18nTextLeft={I18N.cancel}
+        i18nTextRight={isEdit ? I18N.save : I18N.edit}
         disabledRight={!isChanged && isEdit}
         onPressRight={onPressRight}
-        colorRight={LIGHT_GRAPHIC_GREEN_1}
-        colorLeft={LIGHT_GRAPHIC_GREEN_1}
+        colorRight={Color.graphicGreen1}
+        colorLeft={Color.graphicGreen1}
       />
       <SettingsAddressBookEdit
         onSubmit={onSubmit}
