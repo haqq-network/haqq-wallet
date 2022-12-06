cedd33793160acea411575fbc48cd6eb896f8df7
Max <maskalyov.maks@gmail.com>
2022-12-06 19:57:08 +0300

    return const MAIN_ACCOUNT_NAME

--- src/variables.ts
+++ src/variables.ts
@@ -105,6 +105,8 @@ export const HR_GRAY = '#EEEEEE';
 
 export const TRANSPARENT = 'transparent';
 
+export const MAIN_ACCOUNT_NAME = 'Main account';
+
 export const MAGIC_CARD_HEIGHT = 0.632835821;
 
 export const IS_ANDROID = Platform.OS === 'android';
