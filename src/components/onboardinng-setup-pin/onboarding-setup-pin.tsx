2d7dfddd546e20a8226a7d8bdfa5510734787b51
Max <maskalyov.maks@gmail.com>
2022-12-09 03:04:10 +0300

    add onboarding-setup-pin component

---
+++ src/components/onboardinng-setup-pin/onboarding-setup-pin.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import {StyleSheet, View} from 'react-native';
+
+import {Color, getColor} from '@app/colors';
+import {NumericKeyboard} from '@app/components/pin/numeric-keyboard';
+import {PopupContainer, Spacer, Text} from '@app/components/ui';
+import {I18N} from '@app/i18n';
+
+interface OnboardingSetupPinProps {
+  onKeyboard: (value: number) => void;
+  pin: string;
+}
+
+export const OnboardingSetupPin = ({
+  onKeyboard,
+  pin,
+}: OnboardingSetupPinProps) => {
+  return (
+    <PopupContainer style={page.container} testID="onboarding-setup-pin">
+      <Text t4 i18n={I18N.onboardingSetupPinSet} style={page.title} />
+      <Text
+        t11
+        color={Color.textBase2}
+        center
+        i18n={I18N.onboardingSetupPinProjectWallet}
+        i18params={{first: '\u00A0', second: '\u00A0'}}
+      />
+      <Spacer style={page.spacer}>
+        <View style={page.dots}>
+          <View style={[page.dot, pin.length >= 1 && page.active]} />
+          <View style={[page.dot, pin.length >= 2 && page.active]} />
+          <View style={[page.dot, pin.length >= 3 && page.active]} />
+          <View style={[page.dot, pin.length >= 4 && page.active]} />
+          <View style={[page.dot, pin.length >= 5 && page.active]} />
+          <View style={[page.dot, pin.length >= 6 && page.active]} />
+        </View>
+      </Spacer>
+      <NumericKeyboard onPress={onKeyboard} />
+    </PopupContainer>
+  );
+};
+
+const page = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 40,
+    paddingBottom: 16,
+    marginHorizontal: 20,
+  },
+  title: {marginBottom: 12},
+  spacer: {justifyContent: 'center', alignItems: 'center'},
+  dots: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  dot: {
+    width: 18,
+    height: 18,
+    backgroundColor: getColor(Color.graphicSecond2),
+    margin: 5,
+    borderRadius: 9,
+    transform: [{scale: 0.66}],
+  },
+  active: {
+    backgroundColor: getColor(Color.textGreen1),
+    transform: [{scale: 1}],
+  },
+});
