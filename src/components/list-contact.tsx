faea6401556169e0b4157f74aef775f90f5e74e3
Macbook <macbook@MacBooks-MacBook-Pro.local>
2022-12-09 10:37:43 +0500

    fix: lint error

--- src/components/list-contact.tsx
+++ src/components/list-contact.tsx
@@ -6,14 +6,6 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {Color, getColor} from '@app/colors';
 import {AddressRow} from '@app/components/address-row';
 import {Icon, Spacer, SwipeableRow} from '@app/components/ui';
-import {AddressRow} from '@app/components/address-row';
-import {PenIcon, Spacer, SwipeableRow, TrashIcon} from '@app/components/ui';
-import {
-  LIGHT_GRAPHIC_BASE_3,
-  LIGHT_GRAPHIC_RED_1,
-  LIGHT_GRAPHIC_SECOND_4,
-} from '@app/variables';
-
 
 export type ContactFlatListProps = Omit<FlatListProps<any>, 'renderItem'>;
 
