b0499190c397b80198758a0130bdb1d90d450858
Rodion Svinukhov <53443744+sleepy-corpse@users.noreply.github.com>
2022-12-10 09:39:33 +0300

    refactor signup-agreement

--- src/screens/signup-agreement.tsx
+++ src/screens/signup-agreement.tsx
@@ -1,18 +1,19 @@
 import React, {useCallback} from 'react';
-
-import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-
-import {CreateAgreement} from '../components/create-agreement';
-import {RootStackParamList} from '../types';
+import {useTypedNavigation, useTypedRoute} from '@app/hooks';
+import {CreateAgreement} from '@app/components/create-agreement';
 
 export const SignUpAgreementScreen = () => {
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-  const route = useRoute<RouteProp<RootStackParamList, 'createAgreement'>>();
+  const navigation = useTypedNavigation();
+  const route = useTypedRoute<'createAgreement'>();
 
   const onPressAgree = useCallback(() => {
     navigation.navigate(route.params.nextScreen ?? 'onboardingSetupPin');
   }, [navigation, route.params.nextScreen]);
 
-  return <CreateAgreement testID="signup_agreement" onDone={onPressAgree} />;
+  return (
+    <CreateAgreement
+      testID="signup_agreement"
+      onDone={onPressAgree}
+    />
+  );
 };
