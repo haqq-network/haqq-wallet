c3f3afac13dfaf7089f161cdae1b9a4ec8e7ec6c
Max <maskalyov.maks@gmail.com>
2022-12-11 05:12:29 +0300

    refactor

--- src/screens/transaction-contact-edit.tsx
+++ src/screens/transaction-contact-edit.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import {RouteProp, useRoute} from '@react-navigation/native';
-
 import {SettingsAddressBookEdit} from '@app/components/settings-address-book-edit';
-import {useContacts, useTypedNavigation} from '@app/hooks';
-import {RootStackParamList} from '@app/types';
+import {useContacts, useTypedNavigation, useTypedRoute} from '@app/hooks';
 
 export const TransactionContactEditScreen = () => {
   const {name, address} =
-    useRoute<RouteProp<RootStackParamList, 'transactionContactEdit'>>().params;
+    useTypedRoute<'transactionContactEdit'>().params;
   const contacts = useContacts();
   const {goBack} = useTypedNavigation();
 
