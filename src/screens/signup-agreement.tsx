bb636afa8c8db71b8c544e1fdfb21601462319a2
Rodion Svinukhov <rodion2014@gmail.com>
2022-12-12 09:27:24 +0300

    refactor(signupAgreement): code (#593)
    * refactor signup-agreement
    
    * refactor
    
    * refactor component
    
    * fix
    
    * useWindowDimensions hook fix
    
    * add variable for height
    
    Co-authored-by: Server Serverlesskiy <raoffonom@icloud.com>

--- src/screens/signup-agreement.tsx
+++ src/screens/signup-agreement.tsx
@@ -1,18 +1,15 @@
 import React, {useCallback} from 'react';
 
-import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-
-import {CreateAgreement} from '../components/create-agreement';
-import {RootStackParamList} from '../types';
+import {CreateAgreement} from '@app/components/create-agreement';
+import {useTypedNavigation, useTypedRoute} from '@app/hooks';
 
 export const SignUpAgreementScreen = () => {
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-  const route = useRoute<RouteProp<RootStackParamList, 'createAgreement'>>();
+  const navigation = useTypedNavigation();
+  const {nextScreen} = useTypedRoute<'createAgreement'>().params;
 
   const onPressAgree = useCallback(() => {
-    navigation.navigate(route.params.nextScreen ?? 'onboardingSetupPin');
-  }, [navigation, route.params.nextScreen]);
+    navigation.navigate(nextScreen ?? 'onboardingSetupPin');
+  }, [navigation, nextScreen]);
 
   return <CreateAgreement testID="signup_agreement" onDone={onPressAgree} />;
 };
