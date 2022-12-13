9cd3fd05cc30111ba549a2ae6dab1d47ac66f6c7
Andrey Makarov <viva.la.akam@gmail.com>
2022-12-13 16:48:49 +0400

    fix(Android): dark mode

--- src/models/user.ts
+++ src/models/user.ts
@@ -80,8 +80,6 @@ export class User extends EventEmitter {
 
     this._systemTheme = Appearance.getColorScheme() as AppTheme;
 
-    console.log('Appearance.getColorScheme()', Appearance.getColorScheme());
-
     Appearance.addChangeListener(this.listenTheme);
 
     AppState.addEventListener('change', this.listenTheme);
@@ -89,8 +87,6 @@ export class User extends EventEmitter {
 
   listenTheme = () => {
     const theme = Appearance.getColorScheme() as AppTheme;
-
-    console.log('listenTheme', theme, this._systemTheme);
 
     if (theme !== this._systemTheme) {
       this._systemTheme = theme;
