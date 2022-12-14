00f74d03afd21f7a59686568d1c55d6dd211edf6
Dmitriy Vasilev <raoffonom@icloud.com>
2022-12-14 22:52:45 +0500

    fix(notification): notification modal implementation

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -1,22 +1,24 @@
 import React, {useCallback, useEffect, useState} from 'react';
 
-import {StyleSheet, View} from 'react-native';
+import {Modal, StyleSheet, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
+import {NotificationMessage} from '@app/components/notification-message';
 import {useApp} from '@app/hooks';
-
-import {NotificationMessage} from './notification-message';
-
-import {generateUUID} from '../utils';
+import {generateUUID} from '@app/utils';
 
 export const Notifications = () => {
   const app = useApp();
   const insets = useSafeAreaInsets();
   const [messages, setMessages] = useState<{message: string; id: string}[]>([]);
+  const [overlap, setOverlap] = useState(false);
 
   useEffect(() => {
-    const subscription = (message: string) => {
-      setMessages(msgs => msgs.concat({message, id: generateUUID()}).slice(-2));
+    const subscription = (message: string, isOverlap = false) => {
+      setOverlap(isOverlap);
+      setMessages(msgs =>
+        msgs.concat({message, id: generateUUID()}).slice(isOverlap ? -1 : -2),
+      );
     };
 
     app.on('notification', subscription);
@@ -33,25 +35,41 @@ export const Notifications = () => {
   return (
     <View
       pointerEvents="none"
-      style={[page.container, {paddingTop: insets.top}]}>
-      {messages.map(({message, id}) => (
-        <NotificationMessage
-          message={message}
-          id={id}
-          onClose={onClose}
-          key={id}
-        />
-      ))}
+      style={[styles.container, {paddingTop: insets.top}]}>
+      {messages.map(({message, id}) =>
+        overlap ? (
+          <Modal key={id} animationType="none" visible transparent>
+            <View style={styles.modal}>
+              <NotificationMessage
+                message={message}
+                id={id}
+                onClose={onClose}
+              />
+            </View>
+          </Modal>
+        ) : (
+          <NotificationMessage
+            message={message}
+            id={id}
+            onClose={onClose}
+            key={id}
+          />
+        ),
+      )}
     </View>
   );
 };
 
-const page = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
+  },
+  modal: {
+    alignItems: 'center',
+    width: '100%',
   },
 });
