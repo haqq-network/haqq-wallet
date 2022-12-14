da185c8ff1b20042aa19dca1484809966dfb6612
Andrey Makarov <viva.la.akam@gmail.com>
2022-12-14 14:38:26 +0400

    feature(RestoreWallet): private key (#628)

--- src/components/singin-restore-wallet/signin-restore-wallet.tsx
+++ src/components/singin-restore-wallet/signin-restore-wallet.tsx
@@ -27,7 +27,10 @@ export const SignInRestore = ({onDoneTry}: SinginRestoreWalletProps) => {
   const [seed, setSeed] = useState('');
 
   const checked = useMemo(
-    () => utils.isValidMnemonic(seed.trim()) || utils.isHexString(seed.trim()),
+    () =>
+      utils.isValidMnemonic(seed.trim()) ||
+      utils.isHexString(seed.trim()) ||
+      utils.isHexString(`0x${seed}`.trim()),
     [seed],
   );
 
@@ -43,10 +46,14 @@ export const SignInRestore = ({onDoneTry}: SinginRestoreWalletProps) => {
     }
   }, [seed, onDoneTry]);
 
+  const onChangeKey = useCallback((key: string) => {
+    setSeed(key.replace(/\n/gm, ''));
+  }, []);
+
   const onPressPaste = useCallback(async () => {
     const text = await Clipboard.getString();
-    setSeed(text);
-  }, [setSeed]);
+    onChangeKey(text.trim());
+  }, [onChangeKey]);
 
   return (
     <ScrollView
@@ -64,7 +71,7 @@ export const SignInRestore = ({onDoneTry}: SinginRestoreWalletProps) => {
           style={page.input}
           label={I18N.signinRestoreWalletTextFieldLabel}
           value={seed}
-          onChangeText={setSeed}
+          onChangeText={onChangeKey}
           multiline
           errorTextI18n={I18N.signinRestoreWalletTextFieldError}
         />
