3a47f770aa277080da1151e90f70b6732997246a
Server Serverlesskiy <raoffonom@icloud.com>
2022-12-15 09:20:48 +0500

    fix(toast): fix shadows (#645)
    * fix(toast): toast notification shadow
    
    * fix(toast): fix shadows

--- src/components/bottom-popups/popup-track-activity/popup-track-activity.tsx
+++ src/components/bottom-popups/popup-track-activity/popup-track-activity.tsx
@@ -14,7 +14,7 @@ import {
 } from '@app/components/ui';
 import {createTheme} from '@app/helpers';
 import {I18N} from '@app/i18n';
-import {SHADOW_COLOR} from '@app/variables';
+import {SHADOW_COLOR_1} from '@app/variables';
 
 export type TrackActivityProps = {
   onClick: () => void;
@@ -116,7 +116,7 @@ const styles = createTheme({
     borderColor: Color.graphicSecond1,
     borderWidth: 1,
     borderRadius: 12,
-    shadowColor: SHADOW_COLOR,
+    shadowColor: SHADOW_COLOR_1,
     shadowOffset: {
       width: 0,
       height: 6,
