ebfa5e3e0593f99a3bbae4f6c1ba6c3fc0069510
Andrey Makarov <viva.la.akam@gmail.com>
2022-12-20 08:37:08 +0400

    feat(SettingsAbout): center bottom text

--- src/components/settings-about.tsx
+++ src/components/settings-about.tsx
@@ -71,6 +71,7 @@ export const SettingsAbout = () => {
       </View>
       <Text
         t11
+        center
         color={Color.textBase1}
         i18n={I18N.settingsAboutRights}
         style={styles.terms}
