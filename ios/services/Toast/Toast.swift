5b68d652c8603490eb4b68c4db5cc1fe28a3475a
Andrey Makarov <viva.la.akam@gmail.com>
2022-12-20 07:42:27 +0400

    fix(Toast): fix crash and maximum elements

--- ios/services/Toast/Toast.swift
+++ ios/services/Toast/Toast.swift
@@ -10,23 +10,30 @@ import ToastViewSwift
 
 @objc(RNToast)
 class RNToast: NSObject {
+  static var cache: [Toast] = [];
   
   @objc
-  static func requiresMainQueueSetup() -> Bool { return false }
+  static func requiresMainQueueSetup() -> Bool { return true }
   
   @objc
   func message(_ message: Optional<String>, theme: Optional<String>) -> Void {
     guard let message = message else {
       return
     }
-    
     let theme = Theme.Theme(rawValue: theme ?? "light") ?? .light
-    
-    let view = CustomToastView(child: CustomTextToastView(message, theme: theme), theme: theme)
-    let toast = Toast.custom(view: view, config: ToastConfiguration.init(displayTime:2))
-    
+
     DispatchQueue.main.async {
+      let view = CustomToastView(child: CustomTextToastView(message, theme: theme), theme: theme)
+      let toast = Toast.custom(view: view, config: ToastConfiguration.init(displayTime:2))
       toast.show()
+      RNToast.cache.append(toast)
+    }
+
+    if RNToast.cache.count > 3 {
+      let first = RNToast.cache.removeFirst();
+      DispatchQueue.main.async {
+        first.close()
+      }
     }
   }
 }
