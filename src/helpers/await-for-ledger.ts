968167ebaf53156380ab81830f467dab6f809a1d
Andrey Makarov <viva.la.akam@gmail.com>
2023-01-24 12:09:13 +0400

    fix: ledger rejected by device

--- src/helpers/await-for-ledger.ts
+++ src/helpers/await-for-ledger.ts
@@ -6,7 +6,12 @@ import {hideModal, showModal} from '@app/helpers/modal';
 
 export const awaitForLedger = (transport: ProviderInterface) => {
   return new Promise<void>((resolve, reject) => {
-    const done = (status: boolean) => {
+    const done = (
+      status: boolean,
+      _message?: string,
+      _name?: string,
+      code?: string,
+    ) => {
       app.off(Events.onCloseModal, onCloseModal);
       hideModal();
       transport.off('signTypedData', done);
@@ -14,13 +19,13 @@ export const awaitForLedger = (transport: ProviderInterface) => {
       if (status) {
         resolve();
       } else {
-        reject('err');
+        reject(code ?? 'err');
       }
     };
 
-    const onCloseModal = (modal: string) => {
+    const onCloseModal = async (modal: string) => {
       if (modal === 'ledger-attention') {
-        done(false);
+        done(false, '', '', '27013');
       }
     };
 
