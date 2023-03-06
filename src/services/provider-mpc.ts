9d9d015b545fc540c854d546294707f694b7693d
Andrey Makarov <viva.la.akam@gmail.com>
2023-03-06 12:18:59 +0400

    sign messages

--- src/services/provider-mpc.ts
+++ src/services/provider-mpc.ts
@@ -1,4 +1,13 @@
-import {ProviderInterface, compressPublicKey} from '@haqq/provider-base';
+import {UnsignedTransaction, serialize} from '@ethersproject/transactions';
+import {
+  BytesLike,
+  ProviderInterface,
+  TransactionRequest,
+  compressPublicKey,
+  hexStringToByteArray,
+  joinSignature,
+  stringToUtf8Bytes,
+} from '@haqq/provider-base';
 import {Provider as ProviderBase} from '@haqq/provider-base/dist/provider';
 import {ProviderBaseOptions} from '@haqq/provider-base/src/types';
 import {
@@ -7,6 +16,7 @@ import {
   generateEntropy,
   generateMnemonicFromEntropy,
   seedFromMnemonic,
+  sign,
 } from '@haqq/provider-web3-utils';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {lagrangeInterpolation} from '@tkey/core';
@@ -18,6 +28,7 @@ import {ShareTransferModule} from '@tkey/share-transfer';
 import TorusStorageLayer from '@tkey/storage-layer-torus';
 import CustomAuth from '@toruslabs/customauth-react-native-sdk';
 import BN from 'bn.js';
+import {hexConcat} from 'ethers/lib/utils';
 import EncryptedStorage from 'react-native-encrypted-storage';
 
 const directParams = {
@@ -126,14 +137,10 @@ async function getSeed(account: string, storage: StorageInterface) {
     .filter(Boolean)
     .map(r => JSON.parse(r as string));
 
-  console.log('seed shares', account, JSON.stringify(shares));
-
   const privKey = lagrangeInterpolation(
     shares.map(s => new BN(s.share.share, 'hex')),
     shares.map(s => new BN(s.share.shareIndex, 'hex')),
   );
-
-  console.log('privKey', privKey);
 
   const mnemonic = await generateMnemonicFromEntropy(privKey.toBuffer());
   const seed = await seedFromMnemonic(mnemonic);
@@ -283,6 +290,122 @@ export class ProviderMpcReactNative
     return resp;
   }
 
+  async signTransaction(
+    hdPath: string,
+    transaction: TransactionRequest,
+  ): Promise<string> {
+    let resp = '';
+    try {
+      const {seed} = await getSeed(
+        this._options.account,
+        this._options.storage,
+      );
+
+      if (!seed) {
+        throw new Error('seed_not_found');
+      }
+
+      const privateKey = await derive(seed, hdPath);
+
+      if (!privateKey) {
+        throw new Error('private_key_not_found');
+      }
+
+      const signature = await sign(
+        privateKey,
+        serialize(transaction as UnsignedTransaction),
+      );
+
+      const sig = hexStringToByteArray(signature);
+
+      resp = serialize(transaction as UnsignedTransaction, sig);
+
+      this.emit('signTransaction', true);
+    } catch (e) {
+      if (e instanceof Error) {
+        this.catchError(e, 'signTransaction');
+      }
+    }
+
+    return resp;
+  }
+
+  async signPersonalMessage(
+    hdPath: string,
+    message: BytesLike | string,
+  ): Promise<string> {
+    let resp = '';
+    try {
+      const {seed} = await getSeed(
+        this._options.account,
+        this._options.storage,
+      );
+      if (!seed) {
+        throw new Error('seed_not_found');
+      }
+
+      const privateKey = await derive(seed, hdPath);
+
+      if (!privateKey) {
+        throw new Error('private_key_not_found');
+      }
+
+      const m = Array.from(
+        typeof message === 'string' ? stringToUtf8Bytes(message) : message,
+      );
+
+      const hash = Buffer.from(
+        [
+          25, 69, 116, 104, 101, 114, 101, 117, 109, 32, 83, 105, 103, 110, 101,
+          100, 32, 77, 101, 115, 115, 97, 103, 101, 58, 10,
+        ].concat(stringToUtf8Bytes(String(message.length)), m),
+      ).toString('hex');
+      const signature = await sign(privateKey, hash);
+      resp = '0x' + joinSignature(signature);
+      this.emit('signTransaction', true);
+    } catch (e) {
+      if (e instanceof Error) {
+        this.catchError(e, 'signTransaction');
+      }
+    }
+
+    return resp;
+  }
+
+  async signTypedData(
+    hdPath: string,
+    domainHash: string,
+    valueHash: string,
+  ): Promise<string> {
+    let response = '';
+    try {
+      const {seed} = await getSeed(
+        this._options.account,
+        this._options.storage,
+      );
+
+      if (!seed) {
+        throw new Error('seed_not_found');
+      }
+
+      const privateKey = await derive(seed, hdPath);
+
+      if (!privateKey) {
+        throw new Error('private_key_not_found');
+      }
+
+      const concatHash = hexConcat(['0x1901', domainHash, valueHash]);
+      response = await sign(privateKey, concatHash);
+      this.emit('signTypedData', true);
+    } catch (e) {
+      if (e instanceof Error) {
+        this.catchError(e, 'signTypedData');
+      }
+    }
+
+    return response;
+  }
+
   async isShareSaved(): Promise<boolean> {
     return this._options.storage.hasItem(`haqq_${this._options.account}`);
   }
@@ -291,8 +414,6 @@ export class ProviderMpcReactNative
     let shareTmp = await EncryptedStorage.getItem(
       `${ITEM_TMP}_${this._options.account}`,
     );
-
-    console.log('shareTmp', shareTmp);
 
     if (shareTmp) {
       await this._options.storage.setItem(
@@ -303,8 +424,6 @@ export class ProviderMpcReactNative
       const file = await this._options.storage.getItem(
         `haqq_${this._options.account}`,
       );
-
-      console.log('file', file);
 
       if (file === shareTmp) {
         await EncryptedStorage.removeItem(
