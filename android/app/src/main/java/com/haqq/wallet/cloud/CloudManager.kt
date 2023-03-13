dfbd9e561cbbfcf5d6adc0bc28eff183bf693d96
Andrey Makarov <viva.la.akam@gmail.com>
2023-03-13 19:45:23 +0400

    fix: android build

--- android/app/src/main/java/com/haqq/wallet/cloud/CloudManager.kt
+++ android/app/src/main/java/com/haqq/wallet/cloud/CloudManager.kt
@@ -4,6 +4,7 @@ import android.view.Gravity
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
 
 class CloudManager(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
