eaab223f42563e2c2a4cf680793d60b90ce85bf2
Andrey Makarov <viva.la.akam@gmail.com>
2023-03-15 09:10:04 +0400

    wip

--- android/app/src/main/java/com/haqq/wallet/cloud/CloudManager.kt
+++ android/app/src/main/java/com/haqq/wallet/cloud/CloudManager.kt
@@ -1,10 +1,21 @@
 package com.haqq.wallet.cloud
 
-import android.view.Gravity
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Scope
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.http.ByteArrayContent
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.drive.Drive
+import com.google.api.services.drive.DriveScopes
+import com.google.api.services.drive.model.File
+import com.haqq.wallet.R
 
 class CloudManager(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -18,14 +29,20 @@ class CloudManager(reactContext: ReactApplicationContext) :
 
   override fun getConstants(): MutableMap<String, Any> {
     val constants = mutableMapOf<String, Any>()
-    constants["isSupported"] = false
-    constants["isEnabled"] = false
+    constants["isSupported"] = true
+    constants["isEnabled"] = this.isUserSignedIn()
     return constants
   }
 
   @ReactMethod
   fun hasItem(key: String, promise: Promise) {
-    promise.reject("0", "hasItem not_implemented")
+    val exists = getFileId(key)
+
+    if (exists != null) {
+      promise.resolve(true)
+    } else {
+      promise.resolve(false)
+    }
   }
 
   @ReactMethod
@@ -40,6 +57,74 @@ class CloudManager(reactContext: ReactApplicationContext) :
 
   @ReactMethod
   fun setItem(key: String, value: String, promise: Promise) {
-    promise.reject("0", "removeItem not_implemented")
+    try {
+      val googleDriveService = getDriveService()
+        ?: return promise.reject("0", "googleDriveService unavailable")
+
+      val exists = getFileId(key)
+      val contentStream = ByteArrayContent.fromString("text/plain", value)
+      if (exists != null) {
+        val contentFile = File()
+        googleDriveService.Files().update(exists, contentFile, contentStream).execute()
+      } else {
+        val gfile = com.google.api.services.drive.model.File()
+        gfile.name = key
+        googleDriveService.Files().create(gfile, contentStream).execute()
+      }
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
   }
+
+  private fun getFileId(key: String): String? {
+    getDriveService()?.let { googleDriveService ->
+      val result = googleDriveService.files().list().apply {
+        spaces = "drive"
+        fields = "files(id, name)"
+        q = "name='${key}'"
+      }.execute()
+
+      if (result.files.size > 1) {
+        return result.files[0].id
+      }
+    }
+
+    return null
+  }
+
+  private fun isUserSignedIn(): Boolean {
+    val account = GoogleSignIn.getLastSignedInAccount(this.reactContext)
+    return account != null
+  }
+
+  private fun getDriveService(): Drive? {
+    val googleAccount = GoogleSignIn.getLastSignedInAccount(this.reactContext)
+    val credential = GoogleAccountCredential.usingOAuth2(
+      this.reactContext, listOf(DriveScopes.DRIVE_FILE)
+    )
+
+    if (googleAccount == null) {
+      val signInOptions: GoogleSignInOptions =
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+          .requestScopes(Scope(DriveScopes.DRIVE_FILE))
+          .build()
+      val client: GoogleSignInClient = GoogleSignIn.getClient(this.reactContext, signInOptions)
+
+      val googleAccount = GoogleSignIn.getSignedInAccountFromIntent(client.signInIntent)
+      credential.selectedAccount = googleAccount.result.account
+
+    } else {
+      credential.selectedAccount = googleAccount.account!!
+    }
+
+    return Drive.Builder(
+      AndroidHttp.newCompatibleTransport(),
+      JacksonFactory.getDefaultInstance(),
+      credential
+    )
+      .setApplicationName(this.reactContext.resources.getString(R.string.app_name))
+      .build()
+  }
+
+
 }
