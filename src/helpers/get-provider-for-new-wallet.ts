4e7f49567e79aab8136817470fa45a298b6a4b39
Andrey Makarov <viva.la.akam@gmail.com>
2023-03-16 12:25:44 +0400

    fix: create account (#942)

--- src/helpers/get-provider-for-new-wallet.ts
+++ src/helpers/get-provider-for-new-wallet.ts
@@ -3,9 +3,32 @@ import {ProviderMpcReactNative} from '@haqq/provider-mpc-react-native';
 
 import {app} from '@app/contexts';
 import {getProviderStorage} from '@app/helpers/get-provider-storage';
+import {
+  serviceProviderOptions,
+  storageLayerOptions,
+} from '@app/services/provider-mpc';
+import {WalletInitialData} from '@app/types';
 
-export async function getProviderForNewWallet() {
+export async function getProviderForNewWallet(params: WalletInitialData) {
   const getPassword = app.getPassword.bind(app);
+
+  if (params) {
+    if (params.type === 'mpc') {
+      const storage = await getProviderStorage('', 'cloud');
+
+      return await ProviderMpcReactNative.initialize(
+        params.mpcPrivateKey,
+        params.mpcSecurityQuestion || null,
+        params.mpcCloudShare || null,
+        null,
+        app.getPassword.bind(app),
+        storage,
+        serviceProviderOptions as any,
+        storageLayerOptions,
+        {},
+      );
+    }
+  }
 
   const keysMpc = await ProviderMpcReactNative.getAccounts();
 
