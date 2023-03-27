45f31f8e5fca3721b4a1a4d2648abf540112097f
iGroza <kirill.ageichenko@haqq.network>
2023-03-27 11:25:37 +0800

    fix(WalletConnect): reject

--- src/hooks/use-wallet-connect-session.ts
+++ src/hooks/use-wallet-connect-session.ts
@@ -2,15 +2,26 @@ import {useEffect, useState} from 'react';
 
 import {SessionTypes} from '@walletconnect/types';
 
+import {usePrevious} from './use-previous';
 import {useWalletConnectSessions} from './use-wallet-connect-sessions';
 
-export const useWalletConnectSession = (topic: string) => {
+export const useWalletConnectSession = (
+  topic: string,
+  onDisconnect?: () => void,
+) => {
   const {activeSessions} = useWalletConnectSessions();
   const [session, setSession] = useState<SessionTypes.Struct>();
+  const prevSession = usePrevious(session);
 
   useEffect(() => {
     setSession(activeSessions?.find?.(it => it.topic === topic));
   }, [activeSessions, topic, setSession]);
+
+  useEffect(() => {
+    if (!session && prevSession) {
+      onDisconnect?.();
+    }
+  }, [onDisconnect, prevSession, session]);
 
   return session;
 };
