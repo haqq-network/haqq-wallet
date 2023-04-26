aebafc0496c6a45101b0e56d350849211a5d0b40
Andrey Makarov <viva.la.akam@gmail.com>
2023-04-26 16:37:29 +0400

    feat: news

---
+++ src/models/news.ts
@@ -0,0 +1,80 @@
+import {realm} from '@app/models';
+
+export class News extends Realm.Object {
+  static schema = {
+    name: 'News',
+    properties: {
+      id: 'string',
+      title: 'string',
+      content: 'string',
+      createdAt: {type: 'date', default: () => new Date()},
+      updatedAt: {type: 'date', default: () => new Date()},
+    },
+    primaryKey: 'account',
+  };
+  id!: string;
+  title!: string;
+  content!: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  static create(id: string, params: Omit<Partial<News>, 'id'>) {
+    const exists = News.getById(id);
+    realm.write(() => {
+      realm.create<News>(
+        News.schema.name,
+        {
+          ...(exists ?? {}),
+          ...params,
+          id: id.toLowerCase(),
+        },
+        Realm.UpdateMode.Modified,
+      );
+    });
+
+    return id;
+  }
+
+  static remove(address: string) {
+    const obj = realm.objectForPrimaryKey<News>(News.schema.name, address);
+
+    if (obj) {
+      realm.write(() => {
+        realm.delete(obj);
+      });
+    }
+  }
+
+  static getAll() {
+    return realm.objects<News>(News.schema.name);
+  }
+
+  static getById(id: string) {
+    return realm.objectForPrimaryKey<News>(News.schema.name, id.toLowerCase());
+  }
+
+  static removeAll() {
+    const contacts = realm.objects<News>(News.schema.name);
+
+    for (const contact of contacts) {
+      realm.write(() => {
+        realm.delete(contact);
+      });
+    }
+  }
+
+  update(params: Partial<News>) {
+    realm.write(() => {
+      realm.create(
+        News.schema.name,
+        {
+          ...this.toJSON(),
+          ...params,
+          updatedAt: new Date(),
+          id: this.id,
+        },
+        Realm.UpdateMode.Modified,
+      );
+    });
+  }
+}
