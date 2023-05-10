004d2528b67d7f9de6621c64bc388079fd5bb269
Andrey Makarov <viva.la.akam@gmail.com>
2023-05-10 15:39:33 +0400

    fix: try to fix wallet connect

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ import {App} from './src/app';
 import './src/event-actions';
 import {Overview} from './src/overview';
 
-const BigInt = require('big-integer');
-
-Object.assign(global, {
-  BigInt: BigInt,
-});
+// const BigInt = require('big-integer');
+//
+// Object.assign(global, {
+//   BigInt: BigInt,
+// });
 
 import './src/event-actions';
 import {DEBUG_VARS} from '@app/debug-vars';
