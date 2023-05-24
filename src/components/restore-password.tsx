5d563f65e87d794729367fecc5e94394a7cec6d1
Kirill Ageychenko <iGroza@users.noreply.github.com>
2023-05-24 15:45:22 +0800

    fix: reset password modal (#1066)
    Co-authored-by: iGroza <xneonxneon1@gmail.com>

--- src/components/restore-password.tsx
+++ src/components/restore-password.tsx
@@ -4,14 +4,14 @@ import {Alert, Animated, Dimensions, StyleSheet} from 'react-native';
 
 import {Color} from '@app/colors';
 import {Events} from '@app/events';
-import {captureException} from '@app/helpers';
+import {captureException, hideModal} from '@app/helpers';
 import {useApp, useWallets} from '@app/hooks';
 import {I18N, getText} from '@app/i18n';
 import {Contact} from '@app/models/contact';
 import {Transaction} from '@app/models/transaction';
 import {HapticEffects, vibrate} from '@app/services/haptic';
 
-import {BottomSheet} from './bottom-sheet';
+import {BottomSheet, BottomSheetRef} from './bottom-sheet';
 import {Button, ButtonVariant, Spacer, Text} from './ui';
 
 const h = Dimensions.get('window').height;
@@ -25,6 +25,7 @@ export const RestorePassword = ({onClose}: RestorePasswordProps) => {
   const wallet = useWallets();
   const app = useApp();
   const pan = useRef(new Animated.Value(1)).current;
+  const bottomSheetRef = useRef<BottomSheetRef>(null);
 
   const onClosePopup = useCallback(() => {
     Animated.timing(pan, {
@@ -65,11 +66,14 @@ export const RestorePassword = ({onClose}: RestorePasswordProps) => {
               Transaction.removeAll();
               Contact.removeAll();
               await app.clean();
+              bottomSheetRef.current?.close?.();
               Animated.timing(pan, {
                 toValue: 1,
                 duration: 250,
                 useNativeDriver: true,
               }).start(() => {
+                hideModal('splash');
+                hideModal('pin');
                 app.emit(Events.onWalletReset);
               });
             } catch (e) {
@@ -83,6 +87,7 @@ export const RestorePassword = ({onClose}: RestorePasswordProps) => {
 
   return (
     <BottomSheet
+      ref={bottomSheetRef}
       onClose={onClosePopup}
       i18nTitle={I18N.restorePasswordForgot}
       closeDistance={closeDistance}>
