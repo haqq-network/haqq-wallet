35a9dbfc1fcccf8aca6cb0cbb158104bce39e90d
Kirill Ageychenko <iGroza@users.noreply.github.com>
2023-06-22 11:00:11 +0800

    feat: jailbreak screen (#1116)
    Co-authored-by: iGroza <xneonxneon1@gmail.com>

--- src/jailbreak.tsx
+++ src/jailbreak.tsx
@@ -1,7 +1,63 @@
 import React from 'react';
 
-import {Text} from '@app/components/ui';
+import {Image, SafeAreaView, View} from 'react-native';
+
+import {Spacer, StatusBarColor, Text} from '@app/components/ui';
+
+import {Color, getColor} from './colors';
+import {createTheme} from './helpers';
+import {useTheme} from './hooks';
+import {I18N} from './i18n';
+import {AppTheme} from './types';
 
 export const Jailbreak = () => {
-  return <Text>Jailbreak</Text>;
+  const theme = useTheme();
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={styles.content}>
+        <Image source={require('@assets/images/jailbreak.png')} />
+        <StatusBarColor
+          barStyle={theme === AppTheme.dark ? 'light-content' : 'dark-content'}
+          backgroundColor={getColor(Color.bg1)}
+        />
+        <Spacer height={48} />
+        <Text center t4 color={Color.textBase3} i18n={I18N.jailbreakTitle} />
+        <Spacer height={12} />
+        <Text
+          center
+          t11
+          color={Color.textBase3}
+          i18n={I18N.jailbreakDescription1}
+        />
+        <Spacer height={24} />
+        <View style={styles.whiteBox}>
+          <Text
+            center
+            t9
+            color={Color.textRed1}
+            i18n={I18N.jailbreakDescription2}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
 };
+
+const styles = createTheme({
+  container: {
+    flex: 1,
+    backgroundColor: Color.graphicRed1,
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteBox: {
+    backgroundColor: Color.bg1,
+    padding: 16,
+    borderRadius: 13,
+  },
+});
