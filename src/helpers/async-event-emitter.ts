a91de21f15775ee94f814b011b64aa690abb6bdf
iGroza <xneonxneon1@gmail.com>
2023-08-02 14:41:22 +0800

    refactor: async event emitter

--- src/helpers/async-event-emitter.ts
+++ src/helpers/async-event-emitter.ts
@@ -1,6 +1,12 @@
 import EventEmitter from 'events';
 
+import {app} from '@app/contexts';
+import {DEBUG_VARS} from '@app/debug-vars';
+import {makeID} from '@app/utils';
+
 import {EventResolverSymbol} from './await-for-event-done';
+
+const logger = Logger.create('AsyncEventEmitter');
 
 /**
  * Class representing an async event emitter.
@@ -39,22 +45,37 @@ export class AsyncEventEmitter extends EventEmitter {
 
   on(eventName: string | symbol, listener: (...args: any[]) => void): this {
     const wrappedListener = async (...args: any[]) => {
-      // check if event called from `awaitForEventDone` function
-      if (args?.length) {
-        const resolver = args[args.length - 1];
-        if (
-          typeof resolver === 'function' &&
-          resolver.prototype.key === EventResolverSymbol
-        ) {
-          // event start
-          try {
-            await listener(...args);
-          } catch (e) {}
-          // event done'
-          return await resolver();
+      const tx = makeID(5);
+
+      try {
+        if (app.isDeveloper && DEBUG_VARS.enableAsyncEventEmitterLogs) {
+          logger.log(new Date(), 'event started', tx, eventName, ...args);
         }
+        // check if event called from `awaitForEventDone` function
+        if (args?.length) {
+          const resolver = args[args.length - 1];
+          if (
+            typeof resolver === 'function' &&
+            resolver.prototype.key === EventResolverSymbol
+          ) {
+            // event start
+            try {
+              await listener(...args);
+            } catch (e) {}
+            // event done'
+            if (app.isDeveloper && DEBUG_VARS.enableAsyncEventEmitterLogs) {
+              logger.log(new Date(), 'async event finished', tx, eventName);
+            }
+            return await resolver();
+          }
+        }
+        await listener(...args);
+        if (app.isDeveloper && DEBUG_VARS.enableAsyncEventEmitterLogs) {
+          logger.log(new Date(), 'event finished', tx, eventName);
+        }
+      } catch (err) {
+        logger.error(new Date(), 'ERROR', tx, eventName, err);
       }
-      return await listener(...args);
     };
     this.listenerMap.set(listener, wrappedListener);
     return super.on(eventName, wrappedListener);
@@ -75,25 +96,41 @@ export class AsyncEventEmitter extends EventEmitter {
 
   once(eventName: string | symbol, listener: (...args: any[]) => void): this {
     const wrappedListener = async (...args: any[]) => {
-      if (args?.length) {
-        const resolver = args[args.length - 1];
-        if (
-          typeof resolver === 'function' &&
-          resolver.prototype.key === EventResolverSymbol
-        ) {
-          try {
-            await listener(...args);
-          } catch (e) {}
-          // remove listeners after the event is done
-          this.removeListener(eventName, listener);
-          this.removeListener(eventName, wrappedListener);
-          return await resolver();
+      const tx = makeID(5);
+
+      try {
+        if (app.isDeveloper && DEBUG_VARS.enableAsyncEventEmitterLogs) {
+          logger.log(new Date(), 'event started', tx, eventName, ...args);
         }
+        if (args?.length) {
+          const resolver = args[args.length - 1];
+          if (
+            typeof resolver === 'function' &&
+            resolver.prototype.key === EventResolverSymbol
+          ) {
+            try {
+              await listener(...args);
+            } catch (e) {}
+            // event done'
+            if (app.isDeveloper && DEBUG_VARS.enableAsyncEventEmitterLogs) {
+              logger.log(new Date(), 'async event finished', tx, eventName);
+            }
+            // remove listeners after the event is done
+            this.removeListener(eventName, listener);
+            this.removeListener(eventName, wrappedListener);
+            return await resolver();
+          }
+        }
+        // remove listeners after the event is done
+        this.removeListener(eventName, listener);
+        this.removeListener(eventName, wrappedListener);
+        await listener(...args);
+        if (app.isDeveloper && DEBUG_VARS.enableAsyncEventEmitterLogs) {
+          logger.log(new Date(), 'event finished', tx, eventName);
+        }
+      } catch (err) {
+        logger.error(new Date(), 'ERROR', tx, eventName, err);
       }
-      // remove listeners after the event is done
-      this.removeListener(eventName, listener);
-      this.removeListener(eventName, wrappedListener);
-      return await listener(...args);
     };
     this.listenerMap.set(listener, wrappedListener);
     return super.once(eventName, wrappedListener);
