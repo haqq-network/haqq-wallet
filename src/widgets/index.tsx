197f89f19301a46cd2d7a976067be5718780b299
Nikita Ragozin <nikita.ragozin@haqq.network>
2023-08-18 17:39:44 +0500

    add: cleanup home-feed and add demo scheme

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,43 +1,46 @@
 import React, {ReactNode, memo} from 'react';
 
-import {ScrollView, Text, View} from 'react-native';
-
-import {createTheme} from '@app/helpers';
-import {IWidget, IWidgetBase} from '@app/types';
+import {IWidget} from '@app/types';
 import {generateUUID} from '@app/utils';
+import {GovernanceWidgetWrapper} from '@app/widgets/governance-widget';
+import {LayoutWidgetWrapper} from '@app/widgets/layout-widget';
+import {RafflesWidgetWrapper} from '@app/widgets/raffles.widget';
+import {StakingWidgetWrapper} from '@app/widgets/staking-widget';
+import {TransactionsShortWidgetWrapper} from '@app/widgets/transactions-short-widget';
+import {TransactionsWidgetWrapper} from '@app/widgets/transactions-widget';
 
 type IWidgetMap = {
   [key in IWidget['component']]: (
-    params: IWidget & {children?: ReactNode},
+    params: Extract<IWidget, {component: key}> & {
+      children?: ReactNode[];
+      deep?: boolean;
+    },
   ) => ReactNode;
 };
 
-const Element = (params: IWidget) => (
-  <View
-    key={generateUUID()}
-    style={{
-      flex: 1,
-      backgroundColor: params?.component === 'Layout' ? 'green' : 'red',
-      margin: 10,
-      flexDirection: params?.direction === 'horizontal' ? 'row' : 'column',
-    }}>
-    <Text>{params.component}</Text>
-    {params?.children}
-  </View>
-);
-
 const WidgetMap: IWidgetMap = {
-  Transactions: params => <Element {...params} />,
-  TransactionsShort: params => <Element {...params} />,
-  Raffles: params => <Element {...params} />,
-  Staking: params => <Element {...params} />,
-  Governance: params => <Element {...params} />,
-  Layout: params => <Element {...params} />,
-  Ad: params => <Element {...params} />,
-  Banner: params => <Element {...params} />,
+  Transactions: params => (
+    <TransactionsWidgetWrapper key={generateUUID()} {...params} />
+  ),
+  TransactionsShort: params => (
+    <TransactionsShortWidgetWrapper key={generateUUID()} {...params} />
+  ),
+  Raffles: params => <RafflesWidgetWrapper key={generateUUID()} {...params} />,
+  Staking: params => <StakingWidgetWrapper key={generateUUID()} {...params} />,
+  Governance: params => (
+    <GovernanceWidgetWrapper key={generateUUID()} {...params} />
+  ),
+  Layout: params => (
+    <LayoutWidgetWrapper
+      key={generateUUID()}
+      deep={false}
+      children={[]}
+      {...params}
+    />
+  ),
+  Ad: () => null,
+  Banner: () => null,
 };
-
-const EmptyComponent = () => <Element {{component: 'Unknown'}} />;
 
 export const WidgetRoot = memo(() => {
   const data = MOCK_DATA;
@@ -45,92 +48,63 @@ export const WidgetRoot = memo(() => {
     return null;
   }
 
-  const renderWidgetsList = (widgets: IWidget[]): ReactNode[] => {
+  const renderWidgetsList = (
+    widgets: IWidget[],
+    deep: boolean = false,
+  ): ReactNode[] => {
     return widgets.map(widget => {
-      const component = WidgetMap[widget.component] || EmptyComponent;
+      const component = WidgetMap[widget.component];
 
       if (widget.component === 'Layout') {
-        return component({
+        const layoutComponent = WidgetMap[widget.component as 'Layout'];
+        return layoutComponent({
           ...widget,
-          children: renderWidgetsList(widget.child),
+          deep,
+          children: renderWidgetsList(widget.child, true),
         });
       }
 
+      //@ts-ignore
       return component(widget);
     });
   };
 
-  return <View style={styles.wrapper}>{renderWidgetsList(data)}</View>;
-});
-
-const styles = createTheme({
-  wrapper: {
-    flex: 1,
-    flexDirection: 'column',
-  },
+  //@ts-ignore
+  return renderWidgetsList(data);
 });
 
 const MOCK_DATA = [
   {
-    component: 'Transactions',
-  },
-  {
-    component: 'TransactionsShort',
-  },
-  {
-    component: 'Raffles',
-  },
-  {
-    component: 'Staking',
-  },
-  {
-    component: 'Governance',
-  },
-  {
     component: 'Layout',
-    direction: 'horizontal',
+    direction: 'vertical',
     child: [
       {
-        component: 'Ad',
-        title: 'string',
-        description: 'string',
-        background_from: 'string',
-        background_to: 'string',
-        background_image: 'optional string',
-        title_color: 'optional string',
-        description_color: 'optional string',
+        component: 'TransactionsShort',
+      },
+      {
+        component: 'Transactions',
+      },
+      {
+        component: 'Raffles',
+      },
+      {
+        component: 'Staking',
       },
       {
         component: 'Governance',
       },
+      {
+        component: 'Layout',
+        direction: 'horizontal',
+        child: [
+          {
+            component: 'Transactions',
+          },
+          {
+            component: 'Transactions',
+          },
+        ],
+      },
     ],
-  },
-  {
-    component: 'Ad',
-    title: 'string',
-    description: 'string',
-    background_from: 'string',
-    background_to: 'string',
-    background_image: 'optional string',
-    title_color: 'optional string',
-    description_color: 'optional string',
-    target: 'string',
-  },
-  {
-    component: 'Banner',
-    title: 'string',
-    description: 'string',
-    button_title: 'string',
-    button_color: 'string',
-    button_background_color: 'string',
-    background_color_from: 'string',
-    background_color_to: 'string',
-    background_image_url: 'string',
-    title_color: 'optional string',
-    description_color: 'optional string',
-    target: 'string',
-  },
-  {
-    test: 123,
   },
 ];
