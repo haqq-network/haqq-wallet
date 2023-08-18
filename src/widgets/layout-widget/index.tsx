4cbc3f8413609517db4a62fa51f7287128715ac7
Nikita Ragozin <nikita.ragozin@haqq.network>
2023-08-18 17:37:55 +0500

    add: Layout widget

---
+++ src/widgets/layout-widget/index.tsx
@@ -0,0 +1,18 @@
+import React, {ReactNode, memo} from 'react';
+
+import {ILayoutWidget} from '@app/types';
+import {LayoutWidget} from '@app/widgets/layout-widget/layout-widget';
+
+const LayoutWidgetWrapper = memo(
+  (props: ILayoutWidget & {children: ReactNode[]; deep: boolean}) => {
+    return (
+      <LayoutWidget
+        deep={props.deep}
+        direction={props.direction}
+        children={props.children}
+      />
+    );
+  },
+);
+
+export {LayoutWidgetWrapper};
