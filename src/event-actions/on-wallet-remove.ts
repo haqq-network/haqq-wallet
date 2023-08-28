ec32040335c41e708ed5629d727de64f60c63481
Kirill Ageychenko <iGroza@users.noreply.github.com>
2023-08-28 16:45:15 +0700

    fix: remove last wallet bug (#1233)
    Co-authored-by: iGroza <xneonxneon1@gmail.com>

--- src/event-actions/on-wallet-remove.ts
+++ src/event-actions/on-wallet-remove.ts
@@ -1,13 +1,27 @@
+import {app} from '@app/contexts';
 import {Events} from '@app/events';
 import {Transaction} from '@app/models/transaction';
 import {VariablesString} from '@app/models/variables-string';
 import {Wallet} from '@app/models/wallet';
 import {Web3BrowserSession} from '@app/models/web3-browser-session';
+import {navigator} from '@app/navigator';
 import {Backend} from '@app/services/backend';
+
+import {onAppReset} from './on-app-reset';
+import {onWalletReset} from './on-wallet-reset';
 
 export async function onWalletRemove(address: string) {
   try {
     const wallets = Wallet.addressList();
+
+    // last wallet removed
+    if (wallets.length === 0) {
+      await onAppReset();
+      await onWalletReset();
+      navigator.reset({
+        routes: [{name: app.isWelcomeNewsEnabled ? 'welcomeNews' : 'welcome'}],
+      });
+    }
 
     const transactions = Transaction.getAll().snapshot();
     const transactionsTo = transactions.filtered(
