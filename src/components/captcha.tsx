a1626fef1ff679e6c4cfba4838987a29db7ce286
Andrey Makarov <viva.la.akam@gmail.com>
2023-08-31 14:21:16 +0400

    feat: oauth captcha (#1253)

--- src/components/captcha.tsx
+++ src/components/captcha.tsx
@@ -5,6 +5,7 @@ import {StyleSheet, View} from 'react-native';
 import {WebViewMessageEvent} from 'react-native-webview';
 
 import {Color, getColor} from '@app/colors';
+import {Ocaptcha} from '@app/components/ocaptcha/ocaptcha';
 import {createTheme, getWindowHeight, getWindowWidth} from '@app/helpers';
 import {useTheme} from '@app/hooks';
 import {AppTheme} from '@app/types';
@@ -26,6 +27,7 @@ export type CaptchaDataTypes = (
 export enum CaptchaType {
   hcaptcha,
   slider,
+  ocaptcha,
   turnstile,
 }
 
@@ -63,6 +65,7 @@ export const Captcha = ({
       <View onTouchEnd={onPressOutside} style={styles.overlay} />
       <First>
         {type === CaptchaType.slider && <SliderCaptcha onData={onData} />}
+        {type === CaptchaType.ocaptcha && <Ocaptcha onData={onData} />}
         {type === CaptchaType.hcaptcha && (
           <>
             <View style={styles.whiteBox} />
