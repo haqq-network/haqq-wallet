ef4a26a9f5ff0e5d51ccadeeab044218ee8d60ba
Ragozin Nikita <nikita.ragozin@haqq.network>
2023-09-04 10:34:17 +0500

    fix: HQM-115 HQM-116 (#1268)
    * fix: transaction model
    
    * add: transaction model migration
    
    * fix: handler and view

--- src/models/index.ts
+++ src/models/index.ts
@@ -15,6 +15,7 @@ import {VariablesDate} from '@app/models/variables-date';
 import {VariablesString} from '@app/models/variables-string';
 import {VestingMetadata} from '@app/models/vesting-metadata';
 import {Wallet} from '@app/models/wallet';
+import {Balance} from '@app/services/balance';
 import {AppTheme, WalletType} from '@app/types';
 import {
   CARD_DEFAULT_STYLE,
@@ -54,7 +55,7 @@ export const realm = new Realm({
     RssNews,
     VestingMetadata,
   ],
-  schemaVersion: 67,
+  schemaVersion: 68,
   onMigration: (oldRealm, newRealm) => {
     logger.log('onMigration', {
       oldRealmVersion: oldRealm.schemaVersion,
@@ -531,6 +532,23 @@ export const realm = new Realm({
         const newObject = newObjects[objectIndex];
         newObject.tmEndpoints = [];
         newObject.evmEndpoints = [];
+      }
+    }
+
+    if (oldRealm.schemaVersion < 68) {
+      logger.log('migration step #22');
+      const oldObjects = oldRealm.objects<{fee: number}>('Transaction');
+      const newObjects = newRealm.objects<{feeHex: string}>('Transaction');
+
+      logger.log({
+        oldObjects: oldObjects.toJSON(),
+        newObjects: newObjects.toJSON(),
+      });
+
+      for (const objectIndex in oldObjects) {
+        const newObject = newObjects[objectIndex];
+        const oldObject = oldObjects[objectIndex];
+        newObject.feeHex = new Balance(oldObject.fee).toHex();
       }
     }
     logger.log('migration finish');
