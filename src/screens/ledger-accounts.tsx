958056831919b54e259a3c79e283431d0e03e0a4
Ragozin Nikita <nikita.ragozin@haqq.network>
2023-10-06 15:08:26 +0500

    feat: ledger choose account (#1356)
    * fix: start script
    
    * add: choose account for ledger

--- src/screens/ledger-accounts.tsx
+++ src/screens/ledger-accounts.tsx
@@ -2,6 +2,7 @@ import React, {useCallback, useEffect, useRef, useState} from 'react';
 
 import {ProviderLedgerReactNative} from '@haqq/provider-ledger-react-native';
 
+import {ChooseAccountTabNames} from '@app/components/choose-account/choose-account';
 import {LedgerAccounts} from '@app/components/ledger-accounts';
 import {app} from '@app/contexts';
 import {awaitForBluetooth} from '@app/helpers/await-for-bluetooth';
@@ -9,7 +10,11 @@ import {useTypedNavigation, useTypedRoute} from '@app/hooks';
 import {Wallet} from '@app/models/wallet';
 import {EthNetwork} from '@app/services';
 import {LedgerAccountItem} from '@app/types';
-import {ETH_HD_SHORT_PATH, LEDGER_APP} from '@app/variables/common';
+import {
+  ETH_HD_SHORT_PATH,
+  LEDGER_APP,
+  LEDGER_HD_PATH_TEMPLATE,
+} from '@app/variables/common';
 
 export const LedgerAccountsScreen = () => {
   const navigation = useTypedNavigation();
@@ -24,12 +29,23 @@ export const LedgerAccountsScreen = () => {
   const [lastIndex, setLastIndex] = useState(0);
   const [loading, setLoading] = useState(false);
   const [addresses, setAddresses] = useState<LedgerAccountItem[]>([]);
+  const [tab, setTab] = useState<ChooseAccountTabNames>(
+    ChooseAccountTabNames.Ledger,
+  );
 
   useEffect(() => {
     return () => {
       provider.abort();
     };
   }, [provider]);
+
+  const onTabChanged = useCallback((item: ChooseAccountTabNames) => {
+    setLastIndex(0);
+    setAddresses([]);
+    setTab(item);
+    loadAccounts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const loadAccounts = useCallback(() => {
     setLoading(true);
@@ -40,15 +56,21 @@ export const LedgerAccountsScreen = () => {
         const addressList: LedgerAccountItem[] = [];
 
         for (let i = lastIndex; i < lastIndex + 5; i += 1) {
-          const data = await provider.getAccountInfo(
-            `${ETH_HD_SHORT_PATH}/${i}`,
-          );
+          let hdPath = '';
+
+          if (tab === ChooseAccountTabNames.Ledger) {
+            hdPath = `${ETH_HD_SHORT_PATH}/${i}`;
+          } else {
+            hdPath = LEDGER_HD_PATH_TEMPLATE.replace('index', String(i));
+          }
+
+          const data = await provider.getAccountInfo(hdPath);
 
           const balance = await EthNetwork.getBalance(data.address);
 
           addressList.push({
             address: data.address.toLowerCase(),
-            hdPath: `${ETH_HD_SHORT_PATH}/${i}`,
+            hdPath,
             publicKey: data.publicKey,
             exists: Wallet.addressList().includes(data.address.toLowerCase()),
             balance: balance.toFloat(),
@@ -65,7 +87,7 @@ export const LedgerAccountsScreen = () => {
         setLoading(false);
       }
     });
-  }, [lastIndex, provider]);
+  }, [lastIndex, provider, tab]);
 
   useEffect(() => {
     if (lastIndex === 0 && !loading) {
@@ -93,6 +115,7 @@ export const LedgerAccountsScreen = () => {
       addresses={addresses}
       loading={loading}
       loadMore={loadAccounts}
+      onTabChanged={onTabChanged}
     />
   );
 };
