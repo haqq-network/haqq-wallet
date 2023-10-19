216a4b2f69f99bab0879588063e44fa75cb27865
Dmitriy Kudasov <dev.kudasov@gmail.com>
2023-10-19 12:24:16 +0300

    feat(HQM-195): QRcode for bech32

--- src/i18n.ts
+++ src/i18n.ts
@@ -804,6 +804,8 @@ export enum I18N {
   browserGeolocationPermissioUseLocationMessage,
   copyAddress,
   copyBech32Address,
+  evmTitle,
+  bech32Title,
 }
 
 export function getText(key: I18N, params?: Record<string, string>): string {
@@ -1752,4 +1754,6 @@ const en: Record<I18N, string> = {
     '"{{hostname}}" is requesting your current location.',
   [I18N.copyAddress]: 'Copy address',
   [I18N.copyBech32Address]: 'Copy bech32 address',
+  [I18N.evmTitle]: 'EVM',
+  [I18N.bech32Title]: 'Bech32',
 };
