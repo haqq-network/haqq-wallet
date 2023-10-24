29d181da0044941e9c9291f84f2e14e574c58021
Kudasov Dmitriy <dev.kudasov@gmail.com>
2023-10-24 14:49:34 +0300

    feat(HQM-197): Random sorting for validators and validator percents power  (#1392)
    * feat(HQM-197): Sort validators list randomly
    
    * feat(HQM-197): Sorting subheader and power percents
    
    * feat(HQM-197): Remove duplicated sort icon
    
    * feat(HQM-197): Update comment
    
    * feat(HQM-197): Fix overlap issue

--- src/helpers/validators-sort.ts
+++ src/helpers/validators-sort.ts
@@ -1,7 +1,25 @@
 import {ValidatorItem} from '@app/types';
 
+/**
+ * @name validatorsSort
+ * @description Sorting for validators based on total power
+ * @param validators
+ */
 export function validatorsSort(validators: ValidatorItem[]) {
   return validators.sort((valA: ValidatorItem, valB: ValidatorItem) => {
     return Number.parseInt(valB.tokens, 10) - Number.parseInt(valA.tokens, 10);
+  });
+}
+
+/**
+ * @name randomValidatorsSort
+ * @description Totally random sorting for validators based on nothing
+ * @param validators
+ */
+export function randomValidatorsSort(validators: ValidatorItem[]) {
+  return validators.sort(() => {
+    const randomB = Math.random();
+    const randomA = Math.random();
+    return randomB - randomA;
   });
 }
