45dc055f74b731ee25f5f9be34d8ed356fa40daa
Dmitriy Kudasov <dev.kudasov@gmail.com>
2023-11-02 12:25:37 +0300

    feat(HQM-156): Fix default value

--- src/helpers/get-remote-balance-value.tsx
+++ src/helpers/get-remote-balance-value.tsx
@@ -1,8 +1,35 @@
-import {Balance, MIN_AMOUNT} from '@app/services/balance';
+import {
+  Balance,
+  MIN_AMOUNT,
+  MIN_GAS_LIMIT,
+  MIN_STAKING_REWARD,
+} from '@app/services/balance';
+import {COSMOS_MIN_AMOUNT, COSMOS_MIN_GAS_LIMIT} from '@app/services/cosmos';
 import {
   RemoteConfig,
   RemoteConfigBalanceTypes,
 } from '@app/services/remote-config';
+
+const getDefaultBalanceValue = <T extends keyof RemoteConfigBalanceTypes>(
+  key: T,
+): Balance => {
+  switch (key) {
+    case 'transfer_min_amount':
+      return MIN_AMOUNT;
+    case 'staking_reward_min_amount':
+      return MIN_STAKING_REWARD;
+    case 'cosmos_min_amount':
+      return COSMOS_MIN_AMOUNT;
+    case 'cosmos_min_gas_limit':
+      return COSMOS_MIN_GAS_LIMIT;
+    case 'eth_min_amount':
+      return MIN_AMOUNT;
+    case 'eth_min_gas_limit':
+      return MIN_GAS_LIMIT;
+    default:
+      return MIN_AMOUNT;
+  }
+};
 
 export const getRemoteBalanceValue = <T extends keyof RemoteConfigBalanceTypes>(
   key: T,
@@ -13,5 +40,5 @@ export const getRemoteBalanceValue = <T extends keyof RemoteConfigBalanceTypes>(
     return new Balance(remoteValue);
   }
 
-  return MIN_AMOUNT;
+  return getDefaultBalanceValue(key);
 };
