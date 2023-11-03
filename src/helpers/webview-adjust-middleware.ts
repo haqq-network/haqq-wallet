d8a44862ebf74931ebd2653f8961c158295280fe
Kirill Ageychenko <iGroza@users.noreply.github.com>
2023-11-03 13:10:02 +0700

    feat: adjust browser middleware (#1430)
    Co-authored-by: iGroza <xneonxneon1@gmail.com>

---
+++ src/helpers/webview-adjust-middleware.ts
@@ -0,0 +1,73 @@
+import {WebViewMessageEvent} from 'react-native-webview';
+
+import {onTrackEvent} from '@app/event-actions/on-track-event';
+import {AdjustEvents} from '@app/types';
+
+import {Whitelist} from './whitelist';
+
+export enum AdjustMiddlewareEventNames {
+  adjustTrackEvent = 'adjustTrackEvent',
+}
+
+type AdjustTrackEvent = {
+  event: AdjustMiddlewareEventNames.adjustTrackEvent;
+  data: {
+    event: AdjustEvents;
+    params: Record<string, string>;
+  };
+};
+
+export type AdjustMiddlewareEvents = AdjustTrackEvent;
+
+const isAdjustMiddlewareEvent = (data: any): data is AdjustMiddlewareEvents => {
+  return Object.values(AdjustMiddlewareEventNames).includes(data?.event);
+};
+
+const getAdjustMiddlewareEventData = (event: WebViewMessageEvent) => {
+  let data = {};
+
+  try {
+    data = JSON.parse(event.nativeEvent?.data);
+  } catch (e) {
+    return null;
+  }
+
+  if (isAdjustMiddlewareEvent(data)) {
+    return data;
+  }
+
+  return null;
+};
+
+export const WebviewAjustMiddleware = {
+  script: `
+        window.__HAQQWALLET__.trackEvent = (event, params) => {
+          window.ReactNativeWebView.postMessage(JSON.stringify({ event: '${AdjustMiddlewareEventNames.adjustTrackEvent}', data: { event, params } }));
+        };
+        true
+    `,
+  handleMessage: async (event: WebViewMessageEvent) => {
+    const eventData = getAdjustMiddlewareEventData(event);
+
+    if (!eventData) {
+      return false;
+    }
+
+    const isAllowedDomain = await Whitelist.checkUrl(event.nativeEvent.url);
+
+    if (!isAllowedDomain) {
+      Logger.warn('WebviewAjustMiddleware: domain is not allowed');
+      return true;
+    }
+
+    const data = eventData.data;
+
+    switch (eventData.event) {
+      case AdjustMiddlewareEventNames.adjustTrackEvent:
+        onTrackEvent(data.event, data.params);
+        return true;
+      default:
+        return false;
+    }
+  },
+};
