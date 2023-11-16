577a82b11044cbad963b97bec1bd683f086fdd00
Ragozin Nikita <nikita.ragozin@haqq.network>
2023-11-16 16:15:48 +0500

    add: sss disabled condition on import page (#1480)

--- src/components/signin-networks/signin-networks.tsx
+++ src/components/signin-networks/signin-networks.tsx
@@ -1,6 +1,7 @@
 import React, {useCallback, useMemo, useState} from 'react';
 
 import {appleAuth} from '@invertase/react-native-apple-authentication';
+import {observer} from 'mobx-react';
 
 import {Color} from '@app/colors';
 import {
@@ -11,9 +12,12 @@ import {
   Spacer,
   Text,
 } from '@app/components/ui';
+import {app} from '@app/contexts';
 import {createTheme} from '@app/helpers';
 import {I18N} from '@app/i18n';
+import {Wallet} from '@app/models/wallet';
 import {SssProviders} from '@app/services/provider-sss';
+import {WalletType} from '@app/types';
 
 import {SocialButton, SocialButtonVariant} from '../social-button';
 
@@ -22,84 +26,100 @@ export type SssNetworksProps = {
   onSkip: () => void;
 };
 
-export const SigninNetworks = ({onLogin, onSkip}: SssNetworksProps) => {
-  const [isApple, setIsApple] = useState(false);
-  const [isGoogle, setIsGoogle] = useState(false);
+export const SigninNetworks = observer(
+  ({onLogin, onSkip}: SssNetworksProps) => {
+    const [isApple, setIsApple] = useState(false);
+    const [isGoogle, setIsGoogle] = useState(false);
 
-  const isLoading = useMemo(() => isApple || isGoogle, [isApple, isGoogle]);
+    const isLoading = useMemo(() => isApple || isGoogle, [isApple, isGoogle]);
 
-  const onPressLoginGoogle = useCallback(async () => {
-    try {
-      setIsGoogle(true);
+    const onPressLoginGoogle = useCallback(async () => {
+      try {
+        setIsGoogle(true);
 
-      await onLogin(SssProviders.google);
-    } finally {
-      setIsGoogle(false);
-    }
-  }, [onLogin]);
+        await onLogin(SssProviders.google);
+      } finally {
+        setIsGoogle(false);
+      }
+    }, [onLogin]);
 
-  const onPressLoginApple = useCallback(async () => {
-    try {
-      setIsApple(true);
+    const onPressLoginApple = useCallback(async () => {
+      try {
+        setIsApple(true);
 
-      await onLogin(SssProviders.apple);
-    } finally {
-      setIsApple(false);
-    }
-  }, [onLogin]);
+        await onLogin(SssProviders.apple);
+      } finally {
+        setIsApple(false);
+      }
+    }, [onLogin]);
 
-  return (
-    <PopupContainer style={styles.container}>
-      <Spacer centered>
-        <LottieWrap
-          source={require('../../../assets/animations/soc-login.json')}
-          style={styles.logo}
-          autoPlay
-          loop
-        />
-      </Spacer>
-      {/* <SocialButton variant={SocialButtonVariant.discord} />
+    const mnemonicWalletsCount = Wallet.getAll().filter(
+      wallet => wallet.type === WalletType.mnemonic,
+    ).length;
+    const sssWalletsCount = Wallet.getAll().filter(
+      wallet => wallet.type === WalletType.sss,
+    ).length;
+    const isSSSDisabled = app.onboarded
+      ? mnemonicWalletsCount >= 1 && sssWalletsCount === 0
+      : false;
+
+    return (
+      <PopupContainer style={styles.container}>
+        <Spacer centered>
+          <LottieWrap
+            source={require('../../../assets/animations/soc-login.json')}
+            style={styles.logo}
+            autoPlay
+            loop
+          />
+        </Spacer>
+        {/* <SocialButton variant={SocialButtonVariant.discord} />
         <Spacer height={10} />
         <SocialButton variant={SocialButtonVariant.twitter} />
         <Spacer height={10} />
         <SocialButton variant={SocialButtonVariant.facebook} /> */}
 
-      {appleAuth.isSupported && (
-        <>
-          <Spacer height={10} />
-          <SocialButton
-            loading={isApple}
-            disabled={isLoading && !isApple}
-            onPress={onPressLoginApple}
-            variant={SocialButtonVariant.apple}
-          />
-        </>
-      )}
+        {appleAuth.isSupported && !isSSSDisabled && (
+          <>
+            <Spacer height={10} />
+            <SocialButton
+              loading={isApple}
+              disabled={isLoading && !isApple}
+              onPress={onPressLoginApple}
+              variant={SocialButtonVariant.apple}
+            />
+          </>
+        )}
 
-      <Spacer height={10} />
-      <SocialButton
-        loading={isGoogle}
-        disabled={isLoading && !isGoogle}
-        onPress={onPressLoginGoogle}
-        variant={SocialButtonVariant.google}
-      />
-      <Spacer height={10} />
-      <Text
-        t15
-        center
-        i18n={I18N.sssNetworkWeb3AuthDescription}
-        color={Color.textBase2}
-      />
-      <Spacer height={28} />
-      <Button
-        onPress={onSkip}
-        i18n={I18N.signinNetworksSkip}
-        variant={ButtonVariant.contained}
-        testID="signin_network_skip"
-      />
-    </PopupContainer>
-  );
-};
+        <Spacer height={10} />
+        {!isSSSDisabled && (
+          <SocialButton
+            loading={isGoogle}
+            disabled={isLoading && !isGoogle}
+            onPress={onPressLoginGoogle}
+            variant={SocialButtonVariant.google}
+          />
+        )}
+        <Spacer height={10} />
+        {!isSSSDisabled && (
+          <Text
+            t15
+            center
+            i18n={I18N.sssNetworkWeb3AuthDescription}
+            color={Color.textBase2}
+          />
+        )}
+        <Spacer height={28} />
+        <Button
+          onPress={onSkip}
+          i18n={I18N.signinNetworksSkip}
+          variant={ButtonVariant.contained}
+          testID="signin_network_skip"
+        />
+      </PopupContainer>
+    );
+  },
+);
 
 const styles = createTheme({
   logo: {
