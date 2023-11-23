142604687e2c0ab692d48edfc2422c478a659f6f
Ragozin Nikita <nikita.ragozin@haqq.network>
2023-11-23 18:55:34 +0500

    fix: sss disabled condition at import flow (#1500)

--- src/screens/HomeStack/HomeFeedStack/home-feed.tsx
+++ src/screens/HomeStack/HomeFeedStack/home-feed.tsx
@@ -1,7 +1,12 @@
-import React, {memo} from 'react';
+import React, {memo, useEffect} from 'react';
 
 import {HomeFeed} from '@app/components/home-feed';
+import {app} from '@app/contexts';
 
 export const HomeFeedScreen = memo(() => {
+  useEffect(() => {
+    app.onboarded = true;
+  }, []);
+
   return <HomeFeed />;
 });
