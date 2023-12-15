44a240b23963e4bfb39e52f53cba78aab90f15fc
Andrey Makarov <viva.la.akam@gmail.com>
2023-12-15 15:45:44 +0400

    fix: app

--- src/screens/HomeStack/SssMigrate/sss-migrate-networks.tsx
+++ src/screens/HomeStack/SssMigrate/sss-migrate-networks.tsx
@@ -3,6 +3,7 @@ import React, {memo, useCallback} from 'react';
 import {METADATA_URL} from '@env';
 
 import {SssMigrateNetworks} from '@app/components/sss-migrate-networks';
+import {app} from '@app/contexts';
 import {getMetadataValueWrapped} from '@app/helpers/wrappers/get-metadata-value';
 import {useTypedNavigation, useTypedRoute} from '@app/hooks';
 import {ErrorHandler} from '@app/models/error-handler';
