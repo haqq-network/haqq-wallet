576ad9dff1aa6f6bf072f38ac7c87aa3208122d8
Nikita Ragozin <nikita.ragozin@haqq.network>
2023-12-26 13:25:37 +0500

    fix: android release build

--- .detoxrc.js
+++ .detoxrc.js
@@ -33,7 +33,7 @@ module.exports = {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
       build:
-        'cd android && FOR_DETOX=true ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
+        'cd android && FOR_DETOX=true ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release -PHAQQ_UPLOAD_STORE_FILE="$HAQQ_UPLOAD_STORE_FILE" -PHAQQ_UPLOAD_STORE_PASSWORD="$HAQQ_UPLOAD_STORE_PASSWORD" -PHAQQ_UPLOAD_KEY_ALIAS="$HAQQ_UPLOAD_KEY_ALIAS" -PHAQQ_UPLOAD_KEY_PASSWORD="$HAQQ_UPLOAD_KEY_PASSWORD"',
     },
   },
   devices: {
