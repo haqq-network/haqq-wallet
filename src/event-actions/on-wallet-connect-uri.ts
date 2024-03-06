67d7d22453269540414d1da2af020bfedb0b694d
Kirill Ageychenko <iGroza@users.noreply.github.com>
2024-03-06 12:59:11 +0700

    feat: WC pairing loading indicator (#1761)
    Co-authored-by: iGroza <xneonxneon1@gmail.com>

--- src/event-actions/on-wallet-connect-uri.ts
+++ src/event-actions/on-wallet-connect-uri.ts
@@ -1,14 +1,23 @@
 import {parseUri} from '@walletconnect/utils';
 
+import {showModal} from '@app/helpers';
 import {I18N} from '@app/i18n';
 import {sendNotification} from '@app/services';
 import {WalletConnect} from '@app/services/wallet-connect';
+import {ModalType} from '@app/types';
+import {sleep} from '@app/utils';
 
 export async function onWalletConnectUri(uri: string) {
-  const {version} = parseUri(uri);
-  if (version === 1) {
-    return sendNotification(I18N.walletConnectNotSupported);
+  const hideModal = showModal(ModalType.loading);
+  try {
+    const {version} = parseUri(uri);
+    if (version === 1) {
+      return sendNotification(I18N.walletConnectNotSupported);
+    }
+    sendNotification(I18N.walletConnectPairPending);
+    await WalletConnect.instance.pair(uri);
+  } finally {
+    await sleep(1000);
+    hideModal();
   }
-  sendNotification(I18N.walletConnectPairPending);
-  await WalletConnect.instance.pair(uri);
 }
