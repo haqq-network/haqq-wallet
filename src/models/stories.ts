f60612cee72a18a719994436ff939aaa8ddf4e13
Ragozin Nikita <nikita.ragozin@haqq.network>
2024-03-07 09:38:41 +0500

    fix: stories cache (#1764)

--- src/models/stories.ts
+++ src/models/stories.ts
@@ -1,15 +1,24 @@
 import {makeAutoObservable, runInAction} from 'mobx';
-import {isHydrated} from 'mobx-persist-store';
+import {isHydrated, makePersistable} from 'mobx-persist-store';
 
 import {Backend} from '@app/services/backend';
+import {storage} from '@app/services/mmkv';
 import {IStory} from '@app/types';
 
 class StoriesStore {
   private data: Record<IStory['id'], IStory> = {};
   isLoading = false;
 
-  constructor() {
+  constructor(shouldSkipPersisting: boolean = false) {
     makeAutoObservable(this);
+    if (!shouldSkipPersisting) {
+      makePersistable(this, {
+        name: this.constructor.name,
+        //@ts-ignore
+        properties: ['data'],
+        storage: storage,
+      });
+    }
   }
 
   fetch = async (skipLoadingFlag = false) => {
@@ -22,11 +31,11 @@ class StoriesStore {
     if (Array.isArray(stories)) {
       const normalizedStories = stories.reduce(
         (prev, cur) => {
-          const existingValue = prev[cur.id];
+          const existingValue = this.data[cur.id];
           prev[cur.id] = {...cur, seen: existingValue?.seen ?? false};
           return prev;
         },
-        Object.assign({}, this.data),
+        {} as Record<IStory['id'], IStory>,
       );
       runInAction(() => {
         this.data = normalizedStories;
@@ -56,5 +65,5 @@ class StoriesStore {
   };
 }
 
-const instance = new StoriesStore();
+const instance = new StoriesStore(Boolean(process.env.JEST_WORKER_ID));
 export {instance as Stories};
