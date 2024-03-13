85024e24fa10a35f4417a2a8210e232498af1c19
Kirill Ageychenko <iGroza@users.noreply.github.com>
2024-03-13 13:30:29 +0700

    fix: ui fixes (#1780)
    * feat: add useSafeAreaFrame hook in CustomHeader for better layout calculation
    
    * feat: add ModalProvider to app version absolute view
    
    * chore: remove redundant declaration for react-native-safe-area-context as it is not needed.
    
    ---------
    
    Co-authored-by: iGroza <xneonxneon1@gmail.com>

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,8 +2,6 @@ declare module 'json-stable-stringify';
 
 declare module 'react-native-markdown-package';
 
-declare module 'react-native-safe-area-context';
-
 declare const Logger: import('@app/services/logger').LoggerService;
 
 declare module 'react-timer-mixin';
