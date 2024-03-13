c3e2ffff8ed711f7adbb376fba5caf9fb0e168cd
Nikita Ragozin <nikita.ragozin@haqq.network>
2024-03-13 14:47:00 +0500

    feat: add should import mnemonic with duplicated word and see valid phrase in settings test

--- e2e/4_settings.test.ts
+++ e2e/4_settings.test.ts
@@ -1,8 +1,10 @@
 import {by, device, element, expect, waitFor} from 'detox';
+import {Wallet} from 'ethers';
 
 import {createWallet} from './helpers/createWallet';
 import {ensureWalletIsVisible} from './helpers/ensureWalletIsVisible';
 import {launchApp} from './helpers/launchApp';
+import {restoreWallet} from './helpers/restoreWallet';
 import {PIN} from './test-variables';
 
 describe('Routine', () => {
@@ -103,15 +105,58 @@ describe('Routine', () => {
     }
     await device.enableSynchronization();
 
-    for (const word of mnemonic_words) {
-      await waitFor(element(by.text(word)))
-        .toBeVisible()
-        .withTimeout(3000);
+    for (let i = 0; i < mnemonic_words.length; i++) {
+      await waitFor(
+        element(by.id(`settings_view_recovery_phrase_word_${i + 1}`)),
+      )
+        .toHaveText(mnemonic_words[i])
+        .withTimeout(1000);
+    }
+  });
+
+  it('should import mnemonic with duplicated word and see valid phrase in settings', async () => {
+    let mnemonic: string[] = [];
+
+    while (mnemonic.length === 0) {
+      const array = Wallet.createRandom().mnemonic.phrase.split(' ');
+      const duplicates = array.filter(
+        (item, index) => array.indexOf(item) !== index,
+      );
+      if (duplicates.length > 0) {
+        mnemonic = array;
+      }
     }
 
-    await element(by.id('go_back')).tap();
-    await element(by.id('go_back')).tap();
-    await element(by.id('go_back')).tap();
-    await element(by.text('Main')).tap();
+    await device.reloadReactNative();
+    await element(by.id('forgot_the_code')).tap();
+    await element(by.id('reset_wallet')).tap();
+    await element(by.label('Reset')).atIndex(0).tap();
+
+    await restoreWallet(mnemonic.join(' '), PIN, 1);
+
+    await element(by.text('Settings')).tap();
+    await element(by.id('settings_manage_accounts')).tap();
+    await element(by.text('Main account')).tap();
+
+    await waitFor(element(by.id('view_recovery_phrase')))
+      .toBeVisible()
+      .whileElement(by.id('account_details'))
+      .scroll(100, 'down');
+
+    await element(by.id('view_recovery_phrase')).tap();
+
+    await device.disableSynchronization();
+    for (const num of PIN.split('')) {
+      await element(by.id(`numeric_keyboard_${num}`)).tap();
+    }
+    await device.enableSynchronization();
+
+    for (let i = 0; i < mnemonic.length; i++) {
+      await waitFor(
+        element(by.id(`settings_view_recovery_phrase_word_${i + 1}`)),
+      )
+        .toHaveText(mnemonic[i])
+        .withTimeout(1000);
+    }
   });
 });
