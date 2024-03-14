8fc38f036c37c62141b5bee52d634fddaa73e4d8
iGroza <xneonxneon1@gmail.com>
2024-03-14 13:38:44 +0700

    fix: android toast

--- src/services/toast.ts
+++ src/services/toast.ts
@@ -1,13 +1,18 @@
 import {FOR_DETOX} from '@env';
-import {NativeModules} from 'react-native';
+import {NativeModules, ToastAndroid} from 'react-native';
 
 import {app} from '@app/contexts';
 import {I18N, getText} from '@app/i18n';
+import {IS_ANDROID} from '@app/variables/common';
 
 const {RNToast} = NativeModules;
 
 export const message = (msg: string) => {
-  RNToast.message(msg, app.currentTheme);
+  if (IS_ANDROID) {
+    ToastAndroid.show(msg, ToastAndroid.SHORT);
+  } else {
+    RNToast.message(msg, app.currentTheme);
+  }
 };
 
 export function sendNotification(text: I18N, params = {}) {
