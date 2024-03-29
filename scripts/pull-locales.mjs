326f829d3857f15f8022a7f43f4807b6e06fb037
Nikita Ragozin <nikita.ragozin@haqq.network>
2024-03-29 12:14:20 +0500

    feat: add pull-locales script

--- scripts/pull-locales.mjs
+++ scripts/pull-locales.mjs
@@ -1,6 +1,26 @@
 #!/usr/bin/env zx
-import {$} from 'zx';
 require('dotenv').config();
+import {$, fs, path, spinner} from 'zx';
 
-await $`brew tap lokalise/cli-2 && brew install lokalise2`;
-await $`lokalise2 --token ${process.env.LOCALISE_KEY} --project-id ${process.env.LOCALISE_PROJECT_ID} file download --format json --unzip-to ./assets/locales`;
+await spinner(
+  'Installing lozalise2 and jq',
+  () => $`brew tap lokalise/cli-2 && brew install lokalise2 && brew install jq`,
+);
+await spinner(
+  'Downloading locales',
+  () =>
+    $`lokalise2 --token ${process.env.LOCALISE_KEY} --project-id ${process.env.LOCALISE_PROJECT_ID} file download --format json --unzip-to ./assets/locales`,
+);
+await spinner('Run linter', () => $`yarn lint:assets`);
+
+const localesDir = './assets/locales';
+const folders = await fs.readdir(localesDir).then(result => result);
+for await (const localeFolder of folders) {
+  const localeFolderPath = path.join(localesDir, localeFolder);
+  const noFileNamePath = `${localeFolderPath}/no_filename.json`;
+  const localePath = `${localeFolderPath}/${localeFolder}.json`;
+  const combinedPath = `${localeFolderPath}/temp.json`;
+  await $`jq -s '.[0] * .[1]' ${noFileNamePath} ${localePath} > ${combinedPath}`.quiet();
+  await $`mv ${combinedPath} ${localePath}`.quiet();
+  await $`rm ${noFileNamePath}`.quiet();
+}
