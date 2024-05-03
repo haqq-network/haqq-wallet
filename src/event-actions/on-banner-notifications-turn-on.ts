d2fd76850609809d3a58affd7a4a419dc0b78abd
Nikita Ragozin <nikita.ragozin@haqq.network>
2024-05-03 10:28:33 +0500

    add: visibility logic

--- src/event-actions/on-banner-notifications-turn-on.ts
+++ src/event-actions/on-banner-notifications-turn-on.ts
@@ -1,12 +1,13 @@
 import {app} from '@app/contexts';
 import {onPushSubscriptionTransactionsSubscribe} from '@app/event-actions/on-push-subscription-transactions-subscribe';
+import {showModal} from '@app/helpers';
 import {Banner} from '@app/models/banner';
 import {navigator} from '@app/navigator';
 import {
   PushNotificationTopicsEnum,
   PushNotifications,
 } from '@app/services/push-notifications';
-import {PopupNotificationBannerId} from '@app/types';
+import {ModalType, PopupNotificationBannerId} from '@app/types';
 
 import {onBannerNotificationsTopicSubscribe} from './on-banner-notifications-topic-subscribe';
 
@@ -14,19 +15,30 @@ export async function onBannerNotificationsTurnOn(
   id: PopupNotificationBannerId,
 ) {
   try {
-    const enabled = await PushNotifications.instance.requestPermissions();
-
-    if (enabled) {
+    const onSuccess = async () => {
       await onPushSubscriptionTransactionsSubscribe();
       await onBannerNotificationsTopicSubscribe(
         'notificationTopic:news',
         PushNotificationTopicsEnum.news,
       );
-    } else if (app.onboarded) {
-      navigator.navigate('settingsNotification');
-    }
+    };
 
-    Banner.remove(id);
+    const onFailed = () => {
+      if (app.onboarded) {
+        navigator.navigate('settingsNotification');
+      }
+    };
+
+    const isBlocked = await PushNotifications.instance.hasPermissionsBlocked();
+    const isAlreadyTaken = await PushNotifications.instance.hasPermission();
+
+    if (!isBlocked && !isAlreadyTaken) {
+      showModal(ModalType.turnOnPushNotifications, {
+        onSuccess,
+        onFailed,
+        onClose: () => Banner.remove(id),
+      });
+    }
   } catch (e) {
     Logger.captureException(e, 'onBannerNotificationsTurnOn', {id});
   }
