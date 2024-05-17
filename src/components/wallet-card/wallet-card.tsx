c2d76dd2dee9bf60f336ab9ae69b417579ede406
Ragozin Nikita <nikita.ragozin@haqq.network>
2024-05-17 13:22:48 +0500

    fix: android cache crash (#1917)
    * add: safe ping invocation
    
    * remove: large padding in language
    
    * fix: WalletCard onAccountInfo press
    
    * fix: migrate to react-native-video-cache
    
    * fix: crash

--- src/components/wallet-card/wallet-card.tsx
+++ src/components/wallet-card/wallet-card.tsx
@@ -1,7 +1,11 @@
 import React, {useMemo, useState} from 'react';
 
 import {SessionTypes} from '@walletconnect/types';
-import {TouchableWithoutFeedback, useWindowDimensions} from 'react-native';
+import {
+  TouchableWithoutFeedback,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 
 import {Card, Spacer} from '@app/components/ui';
 import {createTheme} from '@app/helpers';
@@ -92,7 +96,7 @@ export const WalletCard = ({
       <TouchableWithoutFeedback
         testID="accountInfoButton"
         onPress={onAccountInfo}>
-        <>
+        <View>
           <BalanceInfoTotal
             isBalancesFirstSync={showPlaceholder}
             total={total}
@@ -103,7 +107,7 @@ export const WalletCard = ({
             total={total}
             locked={locked}
           />
-        </>
+        </View>
       </TouchableWithoutFeedback>
       <Spacer height={12} />
       <CardButtons
