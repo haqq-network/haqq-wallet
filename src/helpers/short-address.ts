9c671b3473b76a58b997ad7ad50bf156fe79f2fc
iGroza <xneonxneon1@gmail.com>
2024-08-12 17:12:45 +0700

    fix: tx sum screen

--- src/helpers/short-address.ts
+++ src/helpers/short-address.ts
@@ -1,4 +1,15 @@
-export function shortAddress(address: string, delimiter: string = '.') {
+export function shortAddress(
+  address: string,
+  delimiter: string = '.',
+  short = false,
+) {
+  if (short) {
+    return `${delimiter.repeat(3)}${address.slice(
+      address.length - 4,
+      address.length,
+    )}`;
+  }
+
   return `${address.slice(0, 4)}${delimiter.repeat(4)}${address.slice(
     address.length - 4,
     address.length,
