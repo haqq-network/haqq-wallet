df95a0a1677df05cb76b812c91e1f21153f9bc43
Kudasov Dmitriy <dev.kudasov@gmail.com>
2024-08-23 15:13:08 +0300

    chore: disable unsupported langs (#2052)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1061,9 +1061,9 @@ export function getText(key: I18N, params?: Record<string, string>): string {
 
 export const supportedTranslationsMap: {[key in AppLanguage]: NodeRequire} = {
   en: require('../assets/locales/en/en.json'),
-  ar: require('../assets/locales/ar/ar.json'),
-  ru: require('../assets/locales/ru/ru.json'),
   tr: require('../assets/locales/tr/tr.json'),
+  // ar: require('../assets/locales/ar/ar.json'),
+  // ru: require('../assets/locales/ru/ru.json'),
 };
 
 /**
