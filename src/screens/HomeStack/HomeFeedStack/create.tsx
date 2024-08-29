b8e124322a8b738a806c32383c4cd2e2db886337
Dmitriy Kudasov <dev.kudasov@gmail.com>
2024-08-29 12:56:17 +0300

    Create and restore translations

--- src/screens/HomeStack/HomeFeedStack/create.tsx
+++ src/screens/HomeStack/HomeFeedStack/create.tsx
@@ -3,22 +3,14 @@ import React, {memo} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {hideBack, popupScreenOptions} from '@app/helpers';
+import {I18N, getText} from '@app/i18n';
 import {basicScreenOptions} from '@app/screens';
 import {OnboardingFinishScreen} from '@app/screens/WelcomeStack/OnboardingStack/onboarding-finish';
 import {SignUpAgreementScreen} from '@app/screens/WelcomeStack/SignUpStack/signup-agreement';
 import {SignUpStoreWalletScreen} from '@app/screens/WelcomeStack/SignUpStack/signup-store-wallet';
-import {MarketingEvents, ScreenOptionType} from '@app/types';
+import {MarketingEvents} from '@app/types';
 
 const SignUpStack = createNativeStackNavigator();
-
-const title = 'Create a wallet';
-const screenOptionsTitle: ScreenOptionType = {
-  ...popupScreenOptions,
-  title,
-  ...hideBack,
-  presentation: 'modal',
-  headerShown: true,
-};
 
 export const CreateStack = memo(props => {
   return (
@@ -28,13 +20,19 @@ export const CreateStack = memo(props => {
         component={SignUpAgreementScreen}
         //@ts-ignore
         initialParams={{nextScreen: 'createStoreWallet', ...props.route.params}}
-        options={screenOptionsTitle}
+        options={{
+          ...popupScreenOptions,
+          title: getText(I18N.welcomeCreateWallet),
+          ...hideBack,
+          presentation: 'modal',
+          headerShown: true,
+        }}
       />
       <SignUpStack.Screen
         name="createStoreWallet"
         component={SignUpStoreWalletScreen}
         options={{
-          title,
+          title: getText(I18N.welcomeCreateWallet),
           header: () => null,
         }}
         initialParams={{action: 'create', nextScreen: 'createFinish'}}
@@ -42,7 +40,13 @@ export const CreateStack = memo(props => {
       <SignUpStack.Screen
         name="createFinish"
         component={OnboardingFinishScreen}
-        options={screenOptionsTitle}
+        options={{
+          ...popupScreenOptions,
+          title: getText(I18N.welcomeCreateWallet),
+          ...hideBack,
+          presentation: 'modal',
+          headerShown: true,
+        }}
         initialParams={{
           action: 'create',
           hide: true,
