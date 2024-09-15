fb72f2ba1d22d857a6df4b6236ec4ccb1271ebe0
Dmitriy Kudasov <dev.kudasov@gmail.com>
2024-09-15 22:02:21 +0300

    network icon for tokens

--- src/components/token-row.tsx
+++ src/components/token-row.tsx
@@ -5,6 +5,7 @@ import {TouchableOpacity, View} from 'react-native';
 import {Color} from '@app/colors';
 import {Spacer, Text, TextVariant} from '@app/components/ui';
 import {createTheme} from '@app/helpers';
+import {Provider} from '@app/models/provider';
 import {IToken} from '@app/types';
 
 import {ImageWrapper} from './image-wrapper';
@@ -20,17 +21,36 @@ export const TokenRow = ({item, checked = false, onPress}: TokenRowProps) => {
     return item?.value?.toFiat?.({fixed: 4});
   }, [item]);
 
+  const providerImage = useMemo(() => {
+    const p = Provider.getByEthChainId(item.chain_id);
+
+    if (!p) {
+      return undefined;
+    }
+
+    return p.icon;
+  }, []);
+
   return (
     <TouchableOpacity
       testID={String(item?.symbol)?.toUpperCase()}
       disabled={!onPress || checked === true}
       onPress={onPress}
       style={[styles.container, checked && styles.checked]}>
-      <ImageWrapper
-        style={styles.icon}
-        source={item?.image || require('@assets/images/empty-icon.png')}
-        resizeMode="cover"
-      />
+      <View>
+        <ImageWrapper
+          style={styles.icon}
+          source={item?.image || require('@assets/images/empty-icon.png')}
+          resizeMode="cover"
+        />
+        {providerImage && (
+          <ImageWrapper
+            style={styles.providerIcon}
+            source={providerImage || require('@assets/images/empty-icon.png')}
+            resizeMode="cover"
+          />
+        )}
+      </View>
       <Spacer width={12} />
       <View style={styles.textContainer}>
         <View style={styles.row}>
@@ -73,6 +93,16 @@ const styles = createTheme({
     width: 42,
     height: 42,
     borderRadius: 12,
+  },
+  providerIcon: {
+    position: 'absolute',
+    top: 0,
+    right: -10,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: Color.bg1,
   },
   textContainer: {
     flex: 1,
