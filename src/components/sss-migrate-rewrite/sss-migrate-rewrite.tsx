a120a8743f6ed087472b01f588006f584c3644da
Dmitriy Kudasov <dev.kudasov@gmail.com>
2024-11-12 11:03:43 +0300

    feat(HW-941): Remove replace option from SSS migration

--- src/components/sss-migrate-rewrite/sss-migrate-rewrite.tsx
+++ src/components/sss-migrate-rewrite/sss-migrate-rewrite.tsx
@@ -1,41 +1,31 @@
-import React, {useCallback, useState} from 'react';
+import React from 'react';
 
 import {Image, StyleSheet, View} from 'react-native';
 
 import {Color} from '@app/colors';
-import {BottomSheet} from '@app/components/bottom-sheet';
 import {
   Button,
   ButtonVariant,
   PopupContainer,
   Spacer,
   Text,
+  TextPosition,
+  TextVariant,
 } from '@app/components/ui';
 import {I18N} from '@app/i18n';
 import {SssProviders} from '@app/services/provider-sss';
-import {capitalize} from '@app/utils';
 
 export type SssMigrateAgreementProps = {
   provider: SssProviders;
   email?: string;
-  onDone: () => void;
   onCancel: () => void;
 };
 
 export const SssMigrateRewrite = ({
   provider,
   email,
-  onDone,
   onCancel,
 }: SssMigrateAgreementProps) => {
-  const [showWarning, setShowWarning] = useState(false);
-  const hideWarningModal = useCallback(() => {
-    setShowWarning(false);
-  }, []);
-  const onPress = useCallback(() => {
-    hideWarningModal();
-    onDone();
-  }, []);
   return (
     <PopupContainer style={styles.container}>
       <View>
@@ -45,24 +35,17 @@ export const SssMigrateRewrite = ({
         />
         <Spacer height={44} />
         <Text
-          t4
-          center
+          variant={TextVariant.t4}
+          position={TextPosition.center}
           i18n={I18N.sssMigrateRewriteTitle}
           i18params={{provider, email: email || ''}}
         />
         <Spacer height={5} />
         <Text
-          t11
+          variant={TextVariant.t11}
           color={Color.textBase2}
-          center
+          position={TextPosition.center}
           i18n={I18N.sssMigrateRewriteDescription}
-        />
-        <Spacer height={5} />
-        <Text
-          t11
-          color={Color.textRed1}
-          center
-          i18n={I18N.sssMigrateRewriteWarning}
         />
       </View>
 
@@ -73,43 +56,7 @@ export const SssMigrateRewrite = ({
           i18n={I18N.sssMigrateRewriteCancel}
           onPress={onCancel}
         />
-        <Button
-          style={styles.button}
-          variant={ButtonVariant.text}
-          textColor={Color.textRed1}
-          i18n={I18N.sssMigrateRewriteRewrite}
-          onPress={() => setShowWarning(true)}
-        />
       </View>
-
-      {!!showWarning && (
-        <BottomSheet
-          fullscreen
-          onClose={hideWarningModal}
-          i18nTitle={I18N.sssReplaceAccountTitle}>
-          <Text
-            i18n={I18N.sssReplaceAccountDescription1}
-            i18params={{provider: capitalize(provider)}}
-            color={Color.textBase2}
-            t11
-            showChildren>
-            <Text
-              t9
-              i18n={I18N.sssReplaceAccountDescription2}
-              color={Color.textBase2}
-            />
-          </Text>
-          <Button
-            error
-            i18n={I18N.sssReplaceAccountButton}
-            onPress={onPress}
-            variant={ButtonVariant.second}
-            textColor={Color.textRed1}
-            style={styles.timerButton}
-            timer={10}
-          />
-        </BottomSheet>
-      )}
     </PopupContainer>
   );
 };
@@ -128,10 +75,6 @@ const styles = StyleSheet.create({
   },
   button: {
     marginBottom: 16,
-  },
-  timerButton: {
-    marginTop: 16,
-    marginBottom: 40,
   },
   buttonContainer: {
     width: '100%',
