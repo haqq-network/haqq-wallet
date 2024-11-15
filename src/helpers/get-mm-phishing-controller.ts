2aaab1825d0d706a3031f0d2c8dcd37609b67eef
Dmitriy Kudasov <dev.kudasov@gmail.com>
2024-11-15 12:28:24 +0300

    Fix types

--- src/helpers/get-mm-phishing-controller.ts
+++ src/helpers/get-mm-phishing-controller.ts
@@ -2,7 +2,7 @@ import {PhishingController} from '@metamask/phishing-controller';
 
 export const getMMPhishingController = () => {
   return new PhishingController({
-    // @ts-ignore
+    //@ts-ignore
     messenger: {
       registerActionHandler: () => {},
       unregisterActionHandler: () => {},
@@ -26,6 +26,7 @@ export const getMMPhishingController = () => {
       ): void {
         throw new Error('Function not implemented.');
       },
+      //@ts-ignore
       '#private': undefined,
     },
   });
