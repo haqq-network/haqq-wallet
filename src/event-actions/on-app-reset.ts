1ef790e0eb6ee54c73d3a7c4f2c0570e16db9b0a
Kirill Ageychenko <iGroza@users.noreply.github.com>
2024-11-25 16:21:18 +0700

    feat(HW-966): immediatly re-create SSS cloud share (#2220)
    * feat(HW-966): immediatly re-create SSS cloud share
    
    * feat(HW-966): sss recovery error handling

--- src/event-actions/on-app-reset.ts
+++ src/event-actions/on-app-reset.ts
@@ -5,6 +5,7 @@ import {resetGenericPassword} from 'react-native-keychain';
 import RNRestart from 'react-native-restart';
 
 import {app} from '@app/contexts';
+import {cleanGoogle} from '@app/helpers/get-google-tokens';
 import {Contact} from '@app/models/contact';
 import {Transaction} from '@app/models/transaction';
 import {VariablesString} from '@app/models/variables-string';
@@ -46,6 +47,7 @@ export async function onAppReset() {
       await EncryptedStorage.setItem('uid', uid);
     }
     app.getUser().resetUserData();
+    cleanGoogle();
   } catch (err) {
     Logger.captureException(err, 'onAppReset');
   } finally {
