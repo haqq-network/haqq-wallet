ac1ad7536dbc360f79d5ba2b926c38ccf4f49387
Kirill Ageychenko <kirill.ageichenko@haqq.network>
2025-02-05 17:33:12 +0700

    e2e: fix Maestro bug when it can’t recognize elements by testId or title

--- src/components/modal-provider.tsx
+++ src/components/modal-provider.tsx
@@ -1,14 +1,16 @@
 import React, {memo} from 'react';
 
-import {Platform, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {FullWindowOverlay} from 'react-native-screens';
+
+import {IS_IOS} from '@app/variables/common';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const ModalProvider = memo(({children}: Props) => {
-  if (Platform.OS === 'ios') {
+  if (IS_IOS) {
     return <FullWindowOverlay>{children}</FullWindowOverlay>;
   }
   return <View style={StyleSheet.absoluteFill}>{children}</View>;
